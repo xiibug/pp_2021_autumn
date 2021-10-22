fd981b7054edfb7f0ba8a5ba8b0998ec3bdda3f6
Nesterov Alexander <nesoldr@gmail.com>
2021-10-22 04:00:03 +0300

    Update ops_mpi.h

--- modules/task_1/nesterov_a_test_task_1/ops_mpi.h
+++ modules/task_1/nesterov_a_test_task_1/ops_mpi.h
@@ -1,6 +1,6 @@
 // Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
-#define MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#ifndef MODULES_TASK_1_NESTEROV_A_TEST_TASK_1_OPS_MPI_H_
+#define MODULES_TASK_1_NESTEROV_A_TEST_TASK_1_OPS_MPI_H_
 
 #include <vector>
 #include <string>
@@ -10,4 +10,4 @@ int getParallelOperations(std::vector<int> global_vec,
                           int count_size_vector, const std::string& ops);
 int getSequentialOperations(std::vector<int> vec, const std::string& ops);
 
-#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#endif  // MODULES_TASK_1_NESTEROV_A_TEST_TASK_1_OPS_MPI_H_
