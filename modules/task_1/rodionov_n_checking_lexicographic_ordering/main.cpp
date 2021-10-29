a7e6323f496516fccaae2c7ac05ec85e6d429cc7
Nikita Rodionov <nikita.rodionov@orioninc.com>
2021-10-29 16:36:18 +0300

    Initial commit

---
+++ modules/task_1/rodionov_n_checking_lexicographic_ordering/main.cpp
@@ -0,0 +1,14 @@
+#include <mpi.h>
+#include <stdio.h>
+#pragma comment (lib, "msmpi.lib")
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+    // Get the rank of the process
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    // Print the message
+    printf("Hello World! My rank is %d\n", my_rank);
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
