75c561ace0f2a9ac3f2ee2a4d4c8334dccf3b1ba
dbakalina <darya.bakalina@gmail.com>
2021-11-04 13:59:04 +0300

    init task

---
+++ modules/task_1/bakalina_d_min_matrix/min_matrix.h
@@ -0,0 +1,7 @@
+// Copyright 2021 Bakalina Darya
+
+void create_random_matrix(int*& mtrx, const unsigned int& size_n, const unsigned int& size_m);
+int lin_search(const int* v, const int len);
+int pharal_search(int* mtrx, int size_n, int size_m);
+
+
