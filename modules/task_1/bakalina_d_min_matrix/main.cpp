b3992bb9035bf4a68bb8f0edc3e63b863750efb9
dbakalina <darya.bakalina@gmail.com>
2021-11-04 18:53:42 +0300

    fix code style mac v2

--- modules/task_1/bakalina_d_min_matrix/main.cpp
+++ modules/task_1/bakalina_d_min_matrix/main.cpp
@@ -26,86 +26,83 @@ TEST(Parallel_Operations_MPI, parallel_search_min_in_matrix_with_size_10_x_10) {
 }
 
 TEST(Parallel_Operations_MPI, parallel_search_min_in_matrix_with_size_100_x_100) {
-	int ProcRank = 0;
-	const int n = 100;
-	const int m = 100;
-	int* mtrx = 0;
-	int ph_min = 0;
+    int ProcRank = 0;
+    const int n = 100;
+    const int m = 100;
+    int* mtrx = 0;
+    int ph_min = 0;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	if (ProcRank == 0) {
-		mtrx = new int[n * m];
-		for (unsigned int i = 0; i < n * m; i++) {
-			mtrx[i] = i + 1;
-		}
-	}
-	ph_min = pharal_search(mtrx, n, m);
-	if (ProcRank == 0) {
-		delete[] mtrx;
-		ASSERT_EQ(ph_min, 1);
-	}
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    if (ProcRank == 0) {
+        mtrx = new int[n * m];
+        for (unsigned int i = 0; i < n * m; i++) {
+             mtrx[i] = i + 1;
+        }
+    }
+    ph_min = pharal_search(mtrx, n, m);
+    if (ProcRank == 0) {
+        delete[] mtrx;
+        ASSERT_EQ(ph_min, 1);
+    }
 }
 
 TEST(Parallel_Operations_MPI, parallel_search_works_in_large_matrix) {
-	int ProcRank = 0;
-	const int n = 1000;
-	const int m = 1000;
-	int* mtrx = 0;
-	int ph_min = 0;
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	if (ProcRank == 0)
-	{
-		mtrx = new int[n * m];
-		for (unsigned int i = 0; i < n * m; i++) {
-			mtrx[i] = i + 1;
-		}
-	}
-	ph_min = pharal_search(mtrx, n, m);
-	if (ProcRank == 0) {
-		delete[] mtrx;
-		ASSERT_EQ(ph_min, 1);
-	}
+    int ProcRank = 0;
+    const int n = 1000;
+    const int m = 1000;
+    int* mtrx = 0;
+    int ph_min = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    if (ProcRank == 0)
+    {
+        mtrx = new int[n * m];
+        for (unsigned int i = 0; i < n * m; i++) {
+             mtrx[i] = i + 1;
+        }
+    }
+    ph_min = pharal_search(mtrx, n, m);
+    if (ProcRank == 0) {
+        delete[] mtrx;
+        ASSERT_EQ(ph_min, 1);
+    }
 }
 
 TEST(Parallel_Operations_MPI, parallel_search_on_random_matrix_with_small_size) {
-	int ProcRank = 0;
-	const int n = 10;
-	const int m = 10;
-	int* mtrx = 0;
-	int ph_min = 0, lin_min = 0;
+    int ProcRank = 0;
+    const int n = 10;
+    const int m = 10;
+    int* mtrx = 0;
+    int ph_min = 0, lin_min = 0;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	if (ProcRank == 0) {
-		create_random_matrix(mtrx, n, m);
-	}
-	ph_min = pharal_search(mtrx, n, m);
-	if (ProcRank == 0) {
-		lin_min = lin_search(mtrx, n * m);
-		delete[] mtrx;
-		ASSERT_EQ(ph_min, lin_min);
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    if (ProcRank == 0) {
+    create_random_matrix(mtrx, n, m);
+    }
+    ph_min = pharal_search(mtrx, n, m);
+    if (ProcRank == 0) {
+        lin_min = lin_search(mtrx, n * m);
+        delete[] mtrx;
+        ASSERT_EQ(ph_min, lin_min);
 	}
 }
 
 TEST(Parallel_Operations_MPI, parallel_search_on_random_matrix_with_large_size) {
-	int ProcRank = 0;
-	const int n = 10000;
-	const int m = 10000;
-	int* mtrx = 0;
-	int ph_min = 0, lin_min = 0;
+    int ProcRank = 0;
+    const int n = 10000;
+    const int m = 10000;
+    int* mtrx = 0;
+    int ph_min = 0, lin_min = 0;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	if (ProcRank == 0) {
-		create_random_matrix(mtrx, n, m);
-	}
-	ph_min = pharal_search(mtrx, n, m);
-	if (ProcRank == 0) {
-		lin_min = lin_search(mtrx, n * m);
-		delete[] mtrx;
-		ASSERT_EQ(ph_min, lin_min);
-
-	}
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    if (ProcRank == 0) {
+        create_random_matrix(mtrx, n, m);
+    }
+    ph_min = pharal_search(mtrx, n, m);
+    if (ProcRank == 0) {
+        lin_min = lin_search(mtrx, n * m);
+        delete[] mtrx;
+        ASSERT_EQ(ph_min, lin_min);
+    }
 }
 
 int main(int argc, char** argv) {
--- modules/task_1/bakalina_d_min_matrix/min_matrix.cpp
+++ modules/task_1/bakalina_d_min_matrix/min_matrix.cpp
@@ -7,63 +7,62 @@
 
 
 int lin_search(const int* v, const int len) {
-	int min;
-	if (len > 0) {
-		min = v[0];
-		for (int i = 0; i < len; ++i) {
-			if (min > v[i])
-				min = v[i];
-		}
-	}
-	else
-		throw "wrong size";
-	return min;
+    int min;
+    if (len > 0) {
+        min = v[0];
+        for (int i = 0; i < len; ++i) {
+             if (min > v[i])
+                 min = v[i];
+        }
+    }
+    else
+      throw "wrong size";
+    return min;
 }
 
 void create_random_matrix(int*& mtrx, const unsigned int& size_n, const unsigned int& size_m) {
-	std::random_device dev;
-	std::mt19937 gen(dev());
-	mtrx = new int[size_n * size_m];
-	for (unsigned int i = 0; i < size_n * size_m; i++) {
-		mtrx[i] = gen() % 1000;
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    mtrx = new int[size_n * size_m];
+    for (unsigned int i = 0; i < size_n * size_m; i++) {
+        mtrx[i] = gen() % 1000;
 		// mtrx[i] = rand() % 10000;
-	}
+    }
 }
 
 int pharal_search(int* mtrx, int size_n, int size_m) {
-	int ProcNum, ProcRank;
-	int quotient, resd;
-	int* rcvbufF, * sd_counts, * disp;
-	int min = 0, glMin = 0;
+    int ProcNum, ProcRank;
+    int quotient, resd;
+    int* rcvbufF, * sd_counts, * disp;
+    int min = 0, glMin = 0;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 
-	quotient = size_n * size_m / ProcNum;
-	resd = size_n * size_m % ProcNum;   
+    quotient = size_n * size_m / ProcNum;
+    resd = size_n * size_m % ProcNum;   
 
+    sd_counts = new int[ProcNum];
+    disp = new int[ProcNum];
 
-	sd_counts = new int[ProcNum];
-	disp = new int[ProcNum];
+    for (int i = 0; i < ProcNum; i++) {
+        sd_counts[i] = quotient;
+        disp[i] = i * quotient;
+    }
+    sd_counts[ProcNum - 1] += resd;
 
-	for (int i = 0; i < ProcNum; i++) {
-		sd_counts[i] = quotient;
-		disp[i] = i * quotient;
-	}
-	sd_counts[ProcNum - 1] += resd;
+    rcvbufF = new int[sd_counts[ProcRank]];
+    MPI_Scatterv(mtrx, sd_counts, disp, MPI_INT, rcvbufF, sd_counts[ProcRank], MPI_INT, 0, MPI_COMM_WORLD);
 
-	rcvbufF = new int[sd_counts[ProcRank]];
-	MPI_Scatterv(mtrx, sd_counts, disp, MPI_INT, rcvbufF, sd_counts[ProcRank], MPI_INT, 0, MPI_COMM_WORLD);
+    min = lin_search(rcvbufF, sd_counts[ProcRank]);
 
-	min = lin_search(rcvbufF, sd_counts[ProcRank]);
+    MPI_Reduce(&min, &glMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 
-	MPI_Reduce(&min, &glMin, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+    if (ProcRank == 0) {
+        delete[] sd_counts;
+        delete[] disp;
+    }
+    delete[] rcvbufF;
 
-	if (ProcRank == 0) {
-		delete[] sd_counts;
-		delete[] disp;
-	}
-	delete[] rcvbufF;
-
-	return glMin;
+    return glMin;
 }
