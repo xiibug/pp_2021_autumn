7dfc985b35fd2d1f618e9b3c889f0c5727e918fc
MikhailZarubin <mikhail.zarubin.itmm@mail.ru>
2021-11-05 11:01:24 +0300

    some_rename

--- modules/task_1/zarubin_m_sum_values_on_rows_matrix/sum_values_on_rows_matrix.h
+++ modules/task_1/zarubin_m_sum_values_on_rows_matrix/sum_values_on_rows_matrix.h
@@ -5,16 +5,16 @@
 #include <vector>
 
 void generateRandomMatrix(std::vector<int>* matrix,
-                          std::vector<int>::size_type count_row,
-                          std::vector<int>::size_type count_column);
+                          std::vector<int>::size_type count_rows,
+                          std::vector<int>::size_type count_columns);
 std::vector<int> transposeMatrix(const std::vector<int>& matrix,
-                                 std::vector<int>::size_type count_row,
-                                 std::vector<int>::size_type count_column);
+                                 std::vector<int>::size_type count_rows,
+                                 std::vector<int>::size_type count_columns);
 std::vector<int> getParallelOperations(const std::vector<int>& matrix,
-                                       std::vector<int>::size_type count_row,
-                                       std::vector<int>::size_type count_column);
+                                       std::vector<int>::size_type count_rows,
+                                       std::vector<int>::size_type count_columns);
 std::vector<int> getSequentialOperations(const std::vector<int>& matrix,
-                                         std::vector<int>::size_type count_row,
-                                         std::vector<int>::size_type count_column);
+                                         std::vector<int>::size_type count_rows,
+                                         std::vector<int>::size_type count_columns);
 
 #endif  // MODULES_TASK_1_ZARUBIN_M_SUM_VALUES_ON_ROWS_MATRIX_SUM_VALUES_ON_ROWS_MATRIX_H_
