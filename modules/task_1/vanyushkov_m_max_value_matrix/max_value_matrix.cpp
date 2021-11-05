8d49edc907722faeeb1cf244197fb624e3e0c511
MaksaLegend <max.van.2001@mail.ru>
2021-11-05 23:55:51 +0300

    Задача 1. Вариант 13. Ванюшков Максим. Максимальное значение элементов матрицы.

--- modules/task_1/vanyushkov_m_max_value_matrix/max_value_matrix.cpp
+++ modules/task_1/vanyushkov_m_max_value_matrix/max_value_matrix.cpp
@@ -1,39 +1,38 @@
 // Copyright 2021 Vanyushkov Maxim
 #include <mpi.h>
 #include <random>
-#include "../../../modules/task_1/vanyushkov_m_max_value_of_table_elements/max_value_of_table.h"
+#include "../../../modules/task_1/vanyushkov_m_max_value_matrix/max_value_matrix.h"
 
 // we will store the matrix in the form of a vector
 // referring to the elements of the matrix has the form:
 // matrix[a][b] -> matrix[i] <=> i = a * size + b
 
 // fill matrix with random values
-void fillMatrixRandValue(int* matrix, int len)
-{
+void fillMatrixRandValue(int* matrix, int len) {
     if (matrix == nullptr)
         throw "matrix is not allocated";
-
     // https://stackoverflow.com/questions/13445688/how-to-generate-a-random-number-in-c
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> distInt(0,0xFFFFFFFF);
-    for (int i = 0; i < len; i++)
+    std::uniform_int_distribution<std::mt19937::result_type> distInt(0, 0xFFFFFFFF);
+    for (int i = 0; i < len; i++) {
         matrix[i] = static_cast<int>(distInt(rng));
+    }
 }
 
 // standard search for max element
-int sequentialMax(const int* matrix, int len)
-{
+int sequentialMax(const int* matrix, int len) {
     int maxValue = matrix[0];
-    for (int i = 1; i < len; i++)
-        if (matrix[i] > maxValue)
+    for (int i = 1; i < len; i++) {
+        if (matrix[i] > maxValue) {
             maxValue = matrix[i];
+        }
+    }
     return maxValue;
 }
 
 // parallel search for max element
-int parallelMax(const int* matrix, int len)
-{
+int parallelMax(const int* matrix, int len) {
     // get MPI info
     int procCount = 0, procRank = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &procCount);
@@ -41,18 +40,18 @@ int parallelMax(const int* matrix, int len)
 
     // scattering the matrix
     int elemsForProc = len / procCount + (procRank < len % procCount ? 1 : 0);
-
+    
     int* scnts = nullptr, * displs = nullptr;
     int* rcount = new int[elemsForProc];
     if (procRank == 0) {
         scnts = new int[procCount];
         for (int i = 0; i < procCount; scnts[i++] = len / procCount) {}
         for (int i = 0; i < len % procCount; ++scnts[i++]) {}
-
         displs = new int[procCount];
         displs[0] = 0;
-        for (int i = 1; i < procCount; i++)
+        for (int i = 1; i < procCount; i++) {
             displs[i] = displs[i - 1] + scnts[i - 1];
+        }
     }
 
     MPI_Scatterv(reinterpret_cast<const void*>(matrix), scnts, displs,
@@ -69,6 +68,8 @@ int parallelMax(const int* matrix, int len)
         delete[] scnts;
         delete[] displs;
     }
+
     delete[] rcount;
     return globalAlterations;
 }
+
--- modules/task_1/vanyushkov_m_max_value_matrix/max_value_matrix.h
+++ modules/task_1/vanyushkov_m_max_value_matrix/max_value_matrix.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Vanyushkov Maxim
-// #ifndef MODULES_TASK_1_VANYUSHKOV_M_MAX_VALUE_OF_TABLE_ELEMENTS_MAX_VALUE_OF_TABLE_H_
-// #define MODULES_TASK_1_VANYUSHKOV_M_MAX_VALUE_OF_TABLE_ELEMENTS_MAX_VALUE_OF_TABLE_H_
+#ifndef MODULES_TASK_1_VANYUSHKOV_M_MAX_VALUE_MATRIX_MAX_VALUE_MATRIX_H_
+#define MODULES_TASK_1_VANYUSHKOV_M_MAX_VALUE_MATRIX_MAX_VALUE_MATRIX_H_
 
 // fill matrix with random values
 void fillMatrixRandValue(int* matrix, int len);
@@ -11,4 +11,5 @@ int sequentialMax(const int* matrix, int len);
 // parallel search for max element
 int parallelMax(const int* matrix, int len);
 
-// #endif  // MODULES_TASK_1_VANYUSHKOV_M_MAX_VALUE_OF_TABLE_ELEMENTS_MAX_VALUE_OF_TABLE_H_
+#endif  // MODULES_TASK_1_VANYUSHKOV_M_MAX_VALUE_OF_TABLE_ELEMENTS_MAX_VALUE_OF_TABLE_H_
+
