540bc5efe98263dfe5874a555a6e99bf3d1835ad
NovozhilovAY <sasha.n191@yandex.ru>
2021-11-06 14:03:36 +0300

    init

---
+++ modules/task_1/novozhilov_a_max_values_in_columns/main.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 Novozhilov Alexander
+#include <gtest/gtest.h>
+#include <vector>
+#include "./max_values_in_columns.h"
+#include <gtest-mpi-listener.hpp>
+
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/novozhilov_a_max_values_in_columns/max_values_in_columns.cpp
@@ -0,0 +1,10 @@
+// Copyright 2021 Novozhilov Alexander
+#include <mpi.h>
+#include <vector>
+#include <string>
+#include <random>
+#include <algorithm>
+#include "../../../modules/task_1/novozhilov_a_max_values_in_columns/max_values_in_columns.h"
+
+
+
