8961d03c8bdb62c5c867c97d914d38d174de3795
multifacs <multifacs@users.noreply.github.com>
2021-11-06 16:21:09 +0300

    fixed tabulation

--- modules/task_1/shelepin_n_col_sum/col_sum.cpp
+++ modules/task_1/shelepin_n_col_sum/col_sum.cpp
@@ -6,74 +6,74 @@
 
 int* randomMatrix(int rows, int cols)
 {
-	if (rows <= 0 || cols <= 0)
-		throw - 1;
+    if (rows <= 0 || cols <= 0)
+        throw - 1;
 
-	int* a = new int[rows * cols];
+    int* a = new int[rows * cols];
 
-	for (int i = 0; i < rows * cols; i++)
-		a[i] = ((double)std::rand() / RAND_MAX) * 10 + 1;
+    for (int i = 0; i < rows * cols; i++)
+        a[i] = ((double)std::rand() / RAND_MAX) * 10 + 1;
 
-	return a;
+    return a;
 }
 
 int* sequentialSum(int* matrix, int rows, int cols)
 {
-	int* answer = new int[cols];
-	for (int i = 0; i < cols; i++)
-		answer[i] = 0;
+    int* answer = new int[cols];
+    for (int i = 0; i < cols; i++)
+        answer[i] = 0;
 
-	for (int j = 0; j < cols; j++)
-		for (int i = 0; i < rows; i++)
-			answer[j] += matrix[j + i * cols];
+    for (int j = 0; j < cols; j++)
+        for (int i = 0; i < rows; i++)
+            answer[j] += matrix[j + i * cols];
 
-	return answer;
+    return answer;
 }
 
 int* parallelSum(int* initial_matrix, int n, int m)
 {
-	if (n <= 0 || m <= 0)
-		throw - 1;
+    if (n <= 0 || m <= 0)
+        throw - 1;
 
-	int rows = 0, cols = 0;
-	int* matrix = nullptr;
-	int* buffer;
-	int* sum, * sum_all;
+    int rows = 0, cols = 0;
+    int* matrix = nullptr;
+    int* buffer;
+    int* sum, * sum_all;
 
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-	if (world_rank == 0)
-	{
-		int empty = 0;
-		if (n % world_size != 0)
-			empty = (n / world_size + 1) * world_size - n;
+    if (world_rank == 0)
+    {
+        int empty = 0;
+        if (n % world_size != 0)
+            empty = (n / world_size + 1) * world_size - n;
 
-		matrix = new int[(n + empty) * m];
+        matrix = new int[(n + empty) * m];
 
-		for (int i = 0; i < n * m; i++)
-			matrix[i] = initial_matrix[i];
-		for (int i = n * m; i < (n + empty) * m; i++)
-			matrix[i] = 0;
+        for (int i = 0; i < n * m; i++)
+            matrix[i] = initial_matrix[i];
+        for (int i = n * m; i < (n + empty) * m; i++)
+            matrix[i] = 0;
 
-		rows = n + empty;
-		cols = m;
-	}
+        rows = n + empty;
+        cols = m;
+    }
 
-	MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	rows = rows / world_size;
-	buffer = new int[rows * cols];
+    rows = rows / world_size;
+    buffer = new int[rows * cols];
 
-	MPI_Scatter(matrix, rows * cols, MPI_INT, buffer, rows * cols, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(matrix, rows * cols, MPI_INT, buffer, rows * cols, MPI_INT, 0, MPI_COMM_WORLD);
 
-	sum_all = new int[cols];
-	sum = sequentialSum(buffer, rows, cols);
+    sum_all = new int[cols];
+    sum = sequentialSum(buffer, rows, cols);
 
-	MPI_Reduce(sum, sum_all, cols, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(sum, sum_all, cols, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-	return sum_all;
+    return sum_all;
 }
