7475c4fa32af9fa234002ad4236793aeb64691f8
Andrey <andrey.zharkov1@yandex.ru>
2021-11-07 20:38:55 +0300

    first commit

---
+++ modules/task_1/zharkov_a_vec_min/vec_min.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include <string>
+
+std::vector<int> getRandomVector(int  sz);
+int getParallelOperations(std::vector<int> global_vec, int count_size_vector);
+int getSequentialOperations(std::vector<int> vec);
