335e475c68abe6fd35e3814b9037cec386406470
Juliaaa-png <juliadenisova107652@gmail.com>
2021-11-08 14:09:08 +0300

    fix3

--- modules/task_1/denisova_max_column_matrix/max_column_matrix.h
+++ modules/task_1/denisova_max_column_matrix/max_column_matrix.h
@@ -7,4 +7,4 @@ void random_matrix(int str, int col, int* matrix);
 void swap_matrix(int str, int col, int* matrix, int* bigmas);
 void max_from_columns(int str, int col, int* bigmas, int* resbuf);
 
-#endif//MODULES_TASK_1_DENISOVA_MAX_COLUMN_MATRIX_MAX_COLUMN_MATRIX_H_
+#endif  // MODULES_TASK_1_DENISOVA_MAX_COLUMN_MATRIX_MAX_COLUMN_MATRIX_H_
