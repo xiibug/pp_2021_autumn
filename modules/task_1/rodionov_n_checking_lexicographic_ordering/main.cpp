575940a0b70914e3af4e4f1f523218bf7861d070
Nikita Rodionov <nikita.rodionov@orioninc.com>
2021-11-11 17:23:02 +0300

    Added ranks validating

--- modules/task_1/rodionov_n_checking_lexicographic_ordering/main.cpp
+++ modules/task_1/rodionov_n_checking_lexicographic_ordering/main.cpp
@@ -16,84 +16,113 @@ int main(int argc, char** argv) {
     listeners.Release(listeners.default_xml_generator());
 
     listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-    return RUN_ALL_TESTS();
+    RUN_ALL_TESTS();
+    return 0;
 }
 
 
 TEST(MPILexicalOrdering, IdenticalTest) {
-    //Generating initial string
-    int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
-    char* string1 = get_random_string(length);
-    char* string2 = new char[length + 1];
-    memcpy(string2, string1, sizeof(char) * (length + 1));
+    int procRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        //Generating initial string
+        int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
+        char* string1 = get_random_string(length);
+        char* string2 = new char[length + 1];
+        memcpy(string2, string1, sizeof(char) * (length + 1));
 
-    int result = omp_lex_compare(string1, string2, length);
+        int result = omp_lex_compare(string1, string2, length);
 
-    ASSERT_EQ(result, 0);
-
+        ASSERT_EQ(result, 0);
+        delete[] string1;
+        delete[] string2;
+    }
 }
 
 TEST(MPILexicalOrdering, GreaterInconsistencyInStartTest) {
-    //Generating initial string
-    int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
-    char* string1 = get_random_string(length);
-    char* string2 = new char[length + 1];
-    memcpy(string2, string1, sizeof(char) * (length + 1));
+    int procRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        //Generating initial string
+        int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
+        char* string1 = get_random_string(length);
+        char* string2 = new char[length + 1];
+        memcpy(string2, string1, sizeof(char) * (length + 1));
 
-    //Adding inconsistency in start
-    string1[0] = 'A';
-    string2[0] = 'B';
+        //Adding inconsistency in start
+        string1[0] = 'A';
+        string2[0] = 'B';
 
-    int result = omp_lex_compare(string1, string2, length);
-    ASSERT_EQ(result, 1);
+        int result = omp_lex_compare(string1, string2, length);
+        ASSERT_EQ(result, 1);
+        delete[] string1;
+        delete[] string2;
+    }
 
 }
 TEST(MPILexicalOrdering, LowerInconsistencyInStartTest) {
-    //Generating initial string
-    int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
-    char* string1 = get_random_string(length);
-    char* string2 = new char[length + 1];
-    memcpy(string2, string1, sizeof(char) * (length + 1));
+    int procRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        //Generating initial string
+        int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
+        char* string1 = get_random_string(length);
+        char* string2 = new char[length + 1];
+        memcpy(string2, string1, sizeof(char) * (length + 1));
 
-    //Adding inconsistency in start
-    string1[0] = 'B';
-    string2[0] = 'A';
+        //Adding inconsistency in start
+        string1[0] = 'B';
+        string2[0] = 'A';
 
-    int result = omp_lex_compare(string1, string2, length);
+        int result = omp_lex_compare(string1, string2, length);
 
-    ASSERT_EQ(result, -1);
+        ASSERT_EQ(result, -1);
+        delete[] string1;
+        delete[] string2;
+    }
 
 }
 
 TEST(MPILexicalOrdering, GreaterInconsistencyInEndTest) {
-    //Generating initial string
-    int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
-    char* string1 = get_random_string(length);
-    char* string2 = new char[length + 1];
-    memcpy(string2, string1, sizeof(char) * (length + 1));
+    int procRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        //Generating initial string
+        int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
+        char* string1 = get_random_string(length);
+        char* string2 = new char[length + 1];
+        memcpy(string2, string1, sizeof(char) * (length + 1));
 
-    //Adding inconsistency in start
-    string1[length - 5] = 'A';
-    string2[length - 5] = 'B';
+        //Adding inconsistency in start
+        string1[length - 5] = 'A';
+        string2[length - 5] = 'B';
 
-    int result = omp_lex_compare(string1, string2, length);
+        int result = omp_lex_compare(string1, string2, length);
 
-    ASSERT_EQ(result, 1);
-
+        ASSERT_EQ(result, 1);
+        delete[] string1;
+        delete[] string2;
+    }
 }
 TEST(MPILexicalOrdering, LowerInconsistencyInEndTest) {
-    //Generating initial string
-    int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
-    char* string1 = get_random_string(length);
-    char* string2 = new char[length + 1];
-    memcpy(string2, string1, sizeof(char) * (length + 1));
+    int procRank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        //Generating initial string
+        int length = rand() * 4000 / RAND_MAX + 1000; // From 1000 to 5000
+        char* string1 = get_random_string(length);
+        char* string2 = new char[length + 1];
+        memcpy(string2, string1, sizeof(char) * (length + 1));
 
-    //Adding inconsistency in start
-    string1[length-5] = 'B';
-    string2[length - 5] = 'A';
+        //Adding inconsistency in start
+        string1[length - 5] = 'B';
+        string2[length - 5] = 'A';
 
-    int result = omp_lex_compare(string1, string2, length);
+        int result = omp_lex_compare(string1, string2, length);
 
-    ASSERT_EQ(result, -1);
+        ASSERT_EQ(result, -1);
+        delete[] string1;
+        delete[] string2;
+    }
 
 }
