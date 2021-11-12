8fb74071e96638a728d329b7cb53e62a8135cdf6
EvgenyPinezhanin <pinezhanin@yandex.ru>
2021-11-12 20:31:48 +0300

    initial commit

---
+++ modules/task_1/pinezhanin_e_trapezoidal_rule/main.cpp
@@ -0,0 +1,19 @@
+// Copyright 2021 Pinezhanin Evgeny
+#include <gtest/gtest.h>
+#include "./trapezoidal_rule.h"
+#include <gtest-mpi-listener.hpp>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/pinezhanin_e_trapezoidal_rule/trapezoidal_rule.cpp
@@ -0,0 +1,4 @@
+// Copyright 2021 Pinezhanin Evgeny
+#include <mpi.h>
+#include "trapezoidal_rule.h"
+#include "../../../modules/task_1/pinezhanin_e_trapezoidal_rule/trapezoidal_rule.h"
