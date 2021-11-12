ac505b096e4a7aa0144ae7eaf072ddfdbfa9282f
Ilya <eeemeeny@gmail.com>
2021-11-12 16:19:30 +0300

    add task 1

---
+++ modules/task_1/kolesnikov_symbol_in_str/kolesnikov_symbol_in_str.h
@@ -0,0 +1,7 @@
+#ifndef KOLESNIKOV_SYMBOL_IN_STR_KOLESNIKOV_SYMBOL_IN_STR_H
+#define KOLESNIKOV_SYMBOL_IN_STR_KOLESNIKOV_SYMBOL_IN_STR_H
+#include <string>
+
+int single_find_symbol(char symbol, std::string str);
+int parallel_find_symbol(char symbol, std::string str);
+#endif 
