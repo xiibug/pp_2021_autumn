005425947edbac8b2b24b7ad55b596d2cac82686
Nikita Rodionov <nikita.rodionov@orioninc.com>
2021-11-12 22:11:41 +0300

    Updated array filling behavior

--- modules/task_1/rodionov_n_checking_lexicographic_ordering/lex_ordering.cpp
+++ modules/task_1/rodionov_n_checking_lexicographic_ordering/lex_ordering.cpp
@@ -48,24 +48,31 @@ int omp_lex_compare(const char* str1, const char* str2, int len) {
 	char* buffer1 = new char[block_size];
 	char* buffer2 = new char[block_size];
 
-	int* global_result = new int[block_size + 1];
-	for (int i = 0; i <= block_size; i++) {
+	
+	
+	int* global_result = new int[commSize+1];
+	for (int i = 0; i < commSize; i++)
 		global_result[i] = 0;
-	}
 
-	//int global_result = 0;
 	MPI_Scatter(str1 + shift, block_size, MPI_CHAR, buffer1, block_size, MPI_CHAR, root, MPI_COMM_WORLD);
 	MPI_Scatter(str2 + shift, block_size, MPI_CHAR, buffer2, block_size, MPI_CHAR, root, MPI_COMM_WORLD);
 	int local_result = 0;
 	local_result = seq_lex_compare(buffer1, buffer2, block_size);
-	MPI_Reduce(reinterpret_cast<void*>(&local_result), reinterpret_cast<void*>(global_result + rank + 1), 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+	if (rank != root) {
+		MPI_Send(&local_result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
 
 	if (rank == root) {
+		for (int i = 1; i < commSize; i++) {
+			MPI_Status status;
+			MPI_Recv(global_result + i + 1,1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+		}
 		global_result[0] = seq_lex_compare(str1, str2, shift);
+		global_result[1] = local_result;
 	}
-	for (int i = 0; i <= block_size; i++) {
+
+	for (int i=0; i < commSize + 1; i++)
 		if (global_result[i] != 0)
 			return global_result[i];
-	}
 	return 0;
 }
