1c3bc970ad5ba23b94c5fcf9388d26f5dde1f046
Nikita Rodionov <nikita.rodionov@orioninc.com>
2021-11-12 21:12:32 +0300

    Fixed test2 &test4

--- modules/task_1/rodionov_n_checking_lexicographic_ordering/lex_ordering.cpp
+++ modules/task_1/rodionov_n_checking_lexicographic_ordering/lex_ordering.cpp
@@ -3,6 +3,7 @@
 #include <mpi.h>
 #include <list>
 #include <cstring>
+#include <iostream>
 const char ALPHABET[26] = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
 
 char* get_random_string(int len) {
@@ -42,23 +43,21 @@ int omp_lex_compare(const char* str1, const char* str2, int len) {
 
 
 	const int block_size = len / commSize;
-	const int shift = len % block_size;
-
+	const int shift = len% block_size;
 
 	char* buffer1 = new char[block_size];
 	char* buffer2 = new char[block_size];
 
-	int global_result;
-	if (rank == root) {
-		memcpy(buffer1, str1, block_size);
-		memcpy(buffer2, str2, block_size);
-		global_result = seq_lex_compare(str1, str2, shift);
-	}
-
+	
+	int global_result = 0;
 	MPI_Scatter(str1 + shift, block_size, MPI_CHAR, buffer1, block_size, MPI_CHAR, root, MPI_COMM_WORLD);
 	MPI_Scatter(str2 + shift, block_size, MPI_CHAR, buffer2, block_size, MPI_CHAR, root, MPI_COMM_WORLD);
 	int local_result = 0;
 	local_result = seq_lex_compare(buffer1, buffer2, block_size);
 	MPI_Reduce(reinterpret_cast<void*>(&local_result), reinterpret_cast<void*>(&global_result), 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+
+	if (rank == root) {
+		global_result += seq_lex_compare(str1, str2, shift);
+	}
 	return global_result;
 }
