047bec74415c34ed3acbe9580e89e94be6f067ac
NovozhilovAY <sasha.n191@yandex.ru>
2021-11-13 14:57:26 +0300

    version 1

--- modules/task_2/novozhilov_a_star_topology/star_topology.h
+++ modules/task_2/novozhilov_a_star_topology/star_topology.h
@@ -1,11 +1,7 @@
 // Copyright 2021 Novozhilov Alexander
 #ifndef MODULES_TASK_2_NOVOZHILOV_A_STAR_TOPOLOGY_STAR_TOPOLOGY_H_
 #define MODULES_TASK_2_NOVOZHILOV_A_STAR_TOPOLOGY_STAR_TOPOLOGY_H_
-
 #include <vector>
-#include <string>
-#include <iostream>
-
-
-
+#include <mpi.h>
+MPI_Comm createStarTopology(MPI_Comm comm, int size);
 #endif  // MODULES_TASK_2_NOVOZHILOV_A_STAR_TOPOLOGY_STAR_TOPOLOGY_H_
