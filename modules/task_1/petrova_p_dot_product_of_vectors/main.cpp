e71d47b685bf002d9d480a7a68425e9c80e4b56c
Polina-Petrova07 <polina_petrova_02@bk.ru>
2021-11-14 19:53:01 +0300

    first commit

---
+++ modules/task_1/petrova_p_dot_product_of_vectors/main.cpp
@@ -0,0 +1,114 @@
+// Copyright 2021 Petrova Polina
+#include <gtest/gtest.h>
+#include <vector>
+#include "./dot_product_of_vectors.h"
+#include <gtest-mpi-listener.hpp>
+
+TEST(ScaulMult_Vectors_MPI, can_create_random_vector_0)
+{
+    std::vector<int> a = fillRandomVector(100);
+    ASSERT_NO_THROW();
+}
+TEST(ScaulMult_Vectors_MPI, can_create_random_vector_1)
+{
+    ASSERT_ANY_THROW(std::vector<int> a=fillRandomVector(-7));
+}
+TEST(ScaulMult_Vectors_MPI, test_0)
+{
+    std::vector<int> a(100, 2), b(100, 1);
+    int res;
+    res = sequentialScalMult(a, b, 100);
+    ASSERT_EQ(res, 200);
+}
+TEST(ScaulMult_Vectors_MPI, test_1)
+{
+    std::vector<int> a(100, 2), b(100, 1);
+    int res;
+    res = sequentialScalMult(a, b, 100);
+    ASSERT_NE(res, 100);
+}
+TEST(ScaulMult_Vectors_MPI, test_2)
+{
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    std::vector<int> a(100, 2), b(100, 1);
+    int res = parallelScalMult(a, b, 100);
+    if (procRank == 0)
+    {
+        ASSERT_EQ(res, 200);
+    }
+}
+TEST(ScaulMult_Vectors_MPI, test_3)
+{
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    std::vector<int> a(100, 2), b(100, 1);
+    int res = parallelScalMult(a, b, 100);
+    if (procRank == 0)
+    {
+        ASSERT_NE(res, 100);
+    }
+}
+TEST(ScaulMult_Vectors_MPI, test_4)
+{
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    const int lenght = 100;
+    std::vector<int> a;
+    std::vector<int> b;
+
+    if (procRank == 0)
+    {
+        a = fillRandomVector(lenght);
+        b = fillRandomVector(lenght);
+    }
+    int sum = parallelScalMult(a, b, lenght);
+
+    if (procRank == 0)
+    {
+        int seqSum = sequentialScalMult(a, b, lenght);
+        EXPECT_EQ(seqSum, sum);
+    }
+}
+TEST(ScaulMult_Vectors_MPI, test_5)
+{
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    const int lenght = 10000;
+    std::vector<int> a;
+    std::vector<int> b;
+
+    if (procRank == 0)
+    {
+        a = fillRandomVector(lenght);
+        b = fillRandomVector(lenght);
+    }
+    int sum = parallelScalMult(a, b, lenght);
+
+    if (procRank == 0)
+    {
+        int seqSum = sequentialScalMult(a, b, lenght);
+        EXPECT_EQ(seqSum, sum);
+    }
+}
+
+
+
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    std::srand(std::time(nullptr));
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
