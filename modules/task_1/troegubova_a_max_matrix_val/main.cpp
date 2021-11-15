97881fa3969565df5a461be6b5f7dc6f27d29e68
Александра Троегубова <troeg1306@gmail.com>
2021-11-15 15:19:28 +0300

    fixed

--- modules/task_1/troegubova_a_max_matrix_val/main.cpp
+++ modules/task_1/troegubova_a_max_matrix_val/main.cpp
@@ -9,7 +9,7 @@ TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     std::vector<int> matrix;
     const int lines = 3;
-	const int columns = 2;
+    const int columns = 2;
 
     if (rank == 0) {
         matrix = getRandomMatrix(lines, columns);
@@ -24,114 +24,114 @@ TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension
 }
 
 TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension_100x100) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> matrix;
-	const int lines = 100;
-	const int columns = 100;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> matrix;
+    const int lines = 100;
+    const int columns = 100;
 
-	if (rank == 0) {
-		matrix = getRandomMatrix(lines, columns);
-	}
+    if (rank == 0) {
+        matrix = getRandomMatrix(lines, columns);
+    }
 
-	int global_max = getParallelMax(matrix, lines * columns);
+    int global_max = getParallelMax(matrix, lines * columns);
 
-	if (rank == 0) {
-		int reference_max = getSequentialMax(matrix);
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix);
 		ASSERT_EQ(reference_max, global_max);
-	}
+    }
 }
 
 TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension_1x150) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> matrix;
-	const int lines = 1;
-	const int columns = 150;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> matrix;
+    const int lines = 1;
+    const int columns = 150;
 
 	if (rank == 0) {
 		matrix = getRandomMatrix(lines, columns);
 	}
 
-	int global_max = getParallelMax(matrix, lines * columns);
+    int global_max = getParallelMax(matrix, lines * columns);
 
-	if (rank == 0) {
-		int reference_max = getSequentialMax(matrix);
-		ASSERT_EQ(reference_max, global_max);
-	}
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix);
+        ASSERT_EQ(reference_max, global_max);
+    }
 }
 
 TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension_150x1) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> matrix;
-	const int lines = 150;
-	const int columns = 1;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> matrix;
+    const int lines = 150;
+    const int columns = 1;
 
-	if (rank == 0) {
-		matrix = getRandomMatrix(lines, columns);
-	}
+    if (rank == 0) {
+        matrix = getRandomMatrix(lines, columns);
+    }
 
-	int global_max = getParallelMax(matrix, lines * columns);
+    int global_max = getParallelMax(matrix, lines * columns);
 
-	if (rank == 0) {
-		int reference_max = getSequentialMax(matrix);
-		ASSERT_EQ(reference_max, global_max);
-	}
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix);
+        ASSERT_EQ(reference_max, global_max);
+    }
 }
 
 TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension_3x200_test_1) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> matrix;
-	const int lines = 3;
-	const int columns = 200;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> matrix;
+    const int lines = 3;
+    const int columns = 200;
 
-	if (rank == 0) {
-		matrix = getRandomMatrix(lines, columns);
-	}
+    if (rank == 0) {
+        matrix = getRandomMatrix(lines, columns);
+    }
 
-	int global_max = getParallelMax(matrix, lines * columns);
+    int global_max = getParallelMax(matrix, lines * columns);
 
-	if (rank == 0) {
-		int reference_max = getSequentialMax(matrix);
-		ASSERT_EQ(reference_max, global_max);
-	}
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix);
+        ASSERT_EQ(reference_max, global_max);
+    }
 }
 
 TEST(parallel_max_MPI, correct_finding_of_the_maximum_in_a_matrix_with_dimension_3x200_test_2) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> matrix;
-	const int lines = 3;
-	const int columns = 200;
-	int matrix_size = lines * columns;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> matrix;
+    const int lines = 3;
+    const int columns = 200;
+    int matrix_size = lines * columns;
 
-	if (rank == 0) {
-		for (int i = 0; i < matrix_size; i++)
-			matrix.push_back(i % 2);
-	}
+    if (rank == 0) {
+        for (int i = 0; i < matrix_size; i++)
+            matrix.push_back(i % 2);
+    }
 
-	int global_max = getParallelMax(matrix, lines * columns);
+    int global_max = getParallelMax(matrix, lines * columns);
 
-	if (rank == 0) {
-		ASSERT_EQ(1, global_max);
-	}
+    if (rank == 0) {
+        ASSERT_EQ(1, global_max);
+    }
 }
 
 
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners =
-		::testing::UnitTest::GetInstance()->listeners();
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
 
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-	return RUN_ALL_TESTS();
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
--- modules/task_1/troegubova_a_max_matrix_val/max_matrix_val.h
+++ modules/task_1/troegubova_a_max_matrix_val/max_matrix_val.h
@@ -8,6 +8,6 @@ std::vector<int> getRandomMatrix(int lines, int columns);
 int getSequentialMax(const std::vector<int> &matrix);
 int getParallelMax(const std::vector<int> &matrix, const int matrix_size);
 
-#endif  //MODULES_TASK_1_TROEGUBOVA_A_MAX_MATRIX_VAL_MAX_MATRIX_VAL_H_
+#endif  // MODULES_TASK_1_TROEGUBOVA_A_MAX_MATRIX_VAL_MAX_MATRIX_VAL_H_
 
 
