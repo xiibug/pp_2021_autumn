b8f96a672900f4bb3363519bc6eb5b9ed371d035
SamoilukAA <samoyluk.anastasia@gmail.com>
2021-11-16 17:35:38 +0300

    version 2

--- modules/task_1/samoiluk_a_number_of_letters/number_of_letters.cpp
+++ modules/task_1/samoiluk_a_number_of_letters/number_of_letters.cpp
@@ -5,11 +5,16 @@
 
 
 int numOfLetters_seq(std::string str) {
-    char cap_let[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-    char sm_let[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    char cap_let[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    char sm_let[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
     int n = 26;
     int count = 0;
-    for (int i = 0; i < str.length(); i++) {
+    int str_l = str.length();
+    for (int i = 0; i < str_l; i++) {
         for (int j = 0; j < n; j++) {
             if (str[i] == cap_let[j] || str[i] == sm_let[j]) {
                 count++;
@@ -21,20 +26,21 @@ int numOfLetters_seq(std::string str) {
 }
 
 int numOfLetters_par(std::string str) {
-    int count = 0; // number of letters
-    int size; // number of processes
-    int rank; // process number
+    int count = 0;  // number of letters
+    int size;  // number of processes
+    int rank;  // process number
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sym_num = 0; // number of symbols to chek for each process
-    int rem = 0; // remaining symbols
-    int proc_count = 0; // number of letters counted by each process
+    int str_l = str.length();  // length of string
+    int sym_num = 0;  // number of symbols to chek for each process
+    int rem = 0;  // remaining symbols
+    int proc_count = 0;  // number of letters counted by each process
 
-    if (size > str.length())
+    if (size > str_l) {
         throw - 1;
-    else {
-        sym_num = str.length() / size;
-        rem = str.length() % size;
+    } else {
+        sym_num = str_l / size;
+        rem = str_l % size;
     }
 
     if (rank == 0) {
@@ -46,9 +52,13 @@ int numOfLetters_par(std::string str) {
             MPI_Send(&f, 1, MPI_INT, pr_rank, 0, MPI_COMM_WORLD);
         }
     }
-     
-    char cap_let[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-    char sm_let[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+    char cap_let[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+        'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    char sm_let[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+        'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
     int n = 26;
     int s, f;
     if (rank != 0) {
@@ -64,8 +74,7 @@ int numOfLetters_par(std::string str) {
                 }
             }
         }
-    }
-    else {
+    } else {
         for (int i = 0; i < (sym_num + rem); i++) {
             for (int j = 0; j < n; j++) {
                 if (str[i] == cap_let[j] || str[i] == sm_let[j]) {
