b86d1419894ca31fab3ddf32bd9d496d68894c44
vorobeva123 <vorobiova.nura@gmail.com>
2021-11-17 00:15:29 +0300

    fix style

--- modules/task_1/vorobyova_a_matrix_sum/main.cpp
+++ modules/task_1/vorobyova_a_matrix_sum/main.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2018 Nesterov Alexander
+// Copyright 2021 Vorobyova Anna
 #include <gtest/gtest.h>
 #include "./matrix_sum.h"
 #include <gtest-mpi-listener.hpp>
@@ -6,90 +6,90 @@
 TEST(Matrix_Sum_MPI, Matrix_3x4) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int* matrix = nullptr; // инициализируем нулевым указателем чтобы не было ошибки
-    const int rows = 3, cols = 4; // строки столбцы
+    int* matrix = nullptr;
+    const int rows = 3, cols = 4;
 
     if (rank == 0) {
-        matrix = getRandomMatrix(rows * cols); // в 0 процессе заполняем матрицу случайными числами
+        matrix = getRandomMatrix(rows * cols);
     }
 
-    int global_sum = getParallelSum(matrix, rows * cols); // запускаем параллельный алгоритм во всех процессах и записываем результат
+    int global_sum = getParallelSum(matrix, rows * cols);
 
-    if (rank == 0) { // в нулевом
-        int reference_sum = getSequentialSum(matrix, rows * cols); // считаем контрольную сумму последовательным алгоритмом
-        ASSERT_EQ(reference_sum, global_sum); // сравниваем контрольную сумму и наш результат
+    if (rank == 0) {
+        int reference_sum = getSequentialSum(matrix, rows * cols);
+        ASSERT_EQ(reference_sum, global_sum);
     }
 }
 
 TEST(Matrix_Sum_MPI, Matrix_5x10) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int* matrix = nullptr; // инициализируем нулевым указателем чтобы не было ошибки
-    const int rows = 5, cols = 10; // строки столбцы
+    int* matrix = nullptr;
+    const int rows = 5, cols = 10;
 
     if (rank == 0) {
-        matrix = getRandomMatrix(rows * cols); // в 0 процессе заполняем матрицу случайными числами
+        matrix = getRandomMatrix(rows * cols);
     }
 
-    int global_sum = getParallelSum(matrix, rows * cols); // запускаем параллельный алгоритм во всех процессах и записываем результат
+    int global_sum = getParallelSum(matrix, rows * cols);
 
-    if (rank == 0) { // в нулевом
-        int reference_sum = getSequentialSum(matrix, rows * cols); // считаем контрольную сумму последовательным алгоритмом
-        ASSERT_EQ(reference_sum, global_sum); // сравниваем контрольную сумму и наш результат
+    if (rank == 0) {
+        int reference_sum = getSequentialSum(matrix, rows * cols);
+        ASSERT_EQ(reference_sum, global_sum);
     }
 }
 
 TEST(Matrix_Sum_MPI, Matrix_10x20) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int* matrix = nullptr; // инициализируем нулевым указателем чтобы не было ошибки
-    const int rows = 10, cols = 20; // строки столбцы
+    int* matrix = nullptr;
+    const int rows = 10, cols = 20;
 
     if (rank == 0) {
-        matrix = getRandomMatrix(rows * cols); // в 0 процессе заполняем матрицу случайными числами
+        matrix = getRandomMatrix(rows * cols);
     }
 
-    int global_sum = getParallelSum(matrix, rows * cols); // запускаем параллельный алгоритм во всех процессах и записываем результат
+    int global_sum = getParallelSum(matrix, rows * cols);
 
-    if (rank == 0) { // в нулевом
-        int reference_sum = getSequentialSum(matrix, rows * cols); // считаем контрольную сумму последовательным алгоритмом
-        ASSERT_EQ(reference_sum, global_sum); // сравниваем контрольную сумму и наш результат
+    if (rank == 0) {
+        int reference_sum = getSequentialSum(matrix, rows * cols);
+        ASSERT_EQ(reference_sum, global_sum);
     }
 }
 
 TEST(Matrix_Sum_MPI, Matrix_30x40) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int* matrix = nullptr; // инициализируем нулевым указателем чтобы не было ошибки
-    const int rows = 30, cols = 40; // строки столбцы
+    int* matrix = nullptr;
+    const int rows = 30, cols = 40;
 
     if (rank == 0) {
-        matrix = getRandomMatrix(rows * cols); // в 0 процессе заполняем матрицу случайными числами
+        matrix = getRandomMatrix(rows * cols);
     }
 
-    int global_sum = getParallelSum(matrix, rows * cols); // запускаем параллельный алгоритм во всех процессах и записываем результат
+    int global_sum = getParallelSum(matrix, rows * cols);
 
-    if (rank == 0) { // в нулевом
-        int reference_sum = getSequentialSum(matrix, rows * cols); // считаем контрольную сумму последовательным алгоритмом
-        ASSERT_EQ(reference_sum, global_sum); // сравниваем контрольную сумму и наш результат
+    if (rank == 0) {
+        int reference_sum = getSequentialSum(matrix, rows * cols);
+        ASSERT_EQ(reference_sum, global_sum);
     }
 }
 
 TEST(Matrix_Sum_MPI, Matrix_50x50) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int* matrix = nullptr; // инициализируем нулевым указателем чтобы не было ошибки
-    const int rows = 50, cols = 50; // строки столбцы
+    int* matrix = nullptr;
+    const int rows = 50, cols = 50;
 
     if (rank == 0) {
-        matrix = getRandomMatrix(rows * cols); // в 0 процессе заполняем матрицу случайными числами
+        matrix = getRandomMatrix(rows * cols);
     }
 
-    int global_sum = getParallelSum(matrix, rows * cols); // запускаем параллельный алгоритм во всех процессах и записываем результат
+    int global_sum = getParallelSum(matrix, rows * cols);
 
-    if (rank == 0) { // в нулевом
-        int reference_sum = getSequentialSum(matrix, rows * cols); // считаем контрольную сумму последовательным алгоритмом
-        ASSERT_EQ(reference_sum, global_sum); // сравниваем контрольную сумму и наш результат
+    if (rank == 0) {
+        int reference_sum = getSequentialSum(matrix, rows * cols);
+        ASSERT_EQ(reference_sum, global_sum);
     }
 }
 
--- modules/task_1/vorobyova_a_matrix_sum/matrix_sum.cpp
+++ modules/task_1/vorobyova_a_matrix_sum/matrix_sum.cpp
@@ -1,67 +1,68 @@
-﻿// Copyright 2018 Nesterov Alexander
+// Copyright 2021 Vorobyova Anna
 #include <mpi.h>
 #include <random>
 #include "../../../modules/task_1/vorobyova_a_matrix_sum/matrix_sum.h"
 
 
-int* getRandomMatrix(int size) { // получить матрицу заполненную случайными числами
+int* getRandomMatrix(int size) {
     std::random_device dev;
-    std::mt19937 gen(dev()); // генератор случайных чисел Нестерова
-    int* matrix = new int[size]; // создать массив
+    std::mt19937 gen(dev());
+    int* matrix = new int[size];
     for (int  i = 0; i < size; i++) {
-        matrix[i] = gen() % 100; // заполнить массив числами не больше 100
+        matrix[i] = gen() % 100;
     }
     return matrix;
 }
 
-int getSequentialSum(int* matrix, int size) { // последовательный алгоритм
-    int sum = 0; // переменная для суммы
+int getSequentialSum(int* matrix, int size) {
+    int sum = 0;
     for (int i = 0; i < size; i++) {
-        sum += matrix[i]; // складываем все элементы
+        sum += matrix[i];
     }
     return sum;
 }
 
-int getParallelSum(int* matrix, int msize) { // параллельный алгоритм
+int getParallelSum(int* matrix, int msize) {
     int size, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &size); // получить количество процессов
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // получить ранг данного процесса
-    const int delta  = msize / size; // размер куска
-    const int remainder = msize % size; // размер остатка
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const int delta  = msize / size;
+    const int remainder = msize % size;
 
-    if (rank == 0) { // отправить матрицу по кускам
-        for (int proc = 1; proc < size; proc++) { // в цикле начинаем с 1 процесса, 0 не трогаем
-            MPI_Send(matrix + proc * delta + remainder, delta, // первый элемент + размер куска * номер процесса
+    if (rank == 0) {
+        for (int proc = 1; proc < size; proc++) {
+            MPI_Send(matrix + proc * delta + remainder, delta,
                         MPI_INT, proc, 0, MPI_COMM_WORLD);
         }
     }
 
-    int* local_matrix = nullptr; // создаем локальную матрицу и записываем нее пустой указатель 
+    int* local_matrix = nullptr;
 
     if (rank == 0) {
-        local_matrix = new int[delta + remainder]; // инициализируем матрицу для хранения бОльшего куска чтобы вместить остаток
+        local_matrix = new int[delta + remainder];
     } else {
-        local_matrix = new int[delta]; // размер куска для остальных процессов
+        local_matrix = new int[delta];
     }
 
     if (rank == 0) {
         for (int i = 0; i < delta + remainder; i++) {
-            local_matrix[i] = matrix[i]; // в цикле копируем в локульную матрицу процесса 0 данные из matrix размера delta + remainder
+            local_matrix[i] = matrix[i];
         }
     } else {
         MPI_Status status;
-        MPI_Recv(local_matrix, delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); // во всех остальных процессах делаем Recv в локальную матрицу
+        MPI_Recv(local_matrix, delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
     }
 
-    int global_sum = 0; // создаем переменную для глобальной суммы
-    int local_sum = 0; // создаем переменную для локальной суммы
+    int global_sum = 0;
+    int local_sum = 0;
 
     if (rank == 0) {
-        local_sum = getSequentialSum(local_matrix, delta + remainder); // считаем сумму в локальной матрице процесса 0
+        local_sum = getSequentialSum(local_matrix, delta + remainder);
     } else {
-        local_sum = getSequentialSum(local_matrix, delta); // считаем сумму в локальных матрицах остальных процессов
+        local_sum = getSequentialSum(local_matrix, delta);
     }
 
-    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); // взять все локальные суммы и сложить в глобальную сумму
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT,
+        MPI_SUM, 0, MPI_COMM_WORLD);
     return global_sum;
 }
