7cf3226eb1a9a2bdb0bc8f128daeced47518ab23
brimst0ne <artemchelebaev@gmail.com>
2021-11-17 21:56:39 +0300

    first lab

---
+++ modules/task_1/chelebaev_a_symbol_frequency/main.cpp
@@ -0,0 +1,94 @@
+// Copyright 2021 Chelebaev Artem
+#include <gtest/gtest.h>
+#include <vector>
+#include "./symbol_frequency.h"
+#include <gtest-mpi-listener.hpp>
+
+
+//----------------------------------------------------
+
+TEST(Parallel_Operations_MPI, parallel_test) {
+    std::string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    char symbol = 'l';
+    if (my_rank == 0) {
+        double freq = parFrequency(&symbol, text);
+        ASSERT_EQ((int)freq, 4);
+    }
+}
+
+TEST(Parallel_Operations_MPI, sequential_test) {
+    std::string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    char symbol = 'l';
+    if (my_rank == 0) {
+        double freq = seqFrequency(&symbol, text);
+        ASSERT_EQ((int)freq, 4);
+    }
+}
+
+TEST(Parallel_Oeration_MPI, par_seq_test) {
+    std::string text = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    char symbol = 'l';
+    if (my_rank == 0) {
+        double freq1 = parFrequency(&symbol, text),
+               freq2 = seqFrequency(&symbol, text);
+        ASSERT_EQ((int)freq1, (int)freq2);
+    }
+}
+
+TEST(Parallel_Operations_MPI, lowercase_test) {
+    std::string text = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    char symbol = 'l';
+    if (my_rank == 0) {
+        double freq = parFrequency(&symbol, text);
+        ASSERT_EQ((int)freq, 4);
+    }
+}
+
+TEST(Parallel_Operations_MPI, uppercase_test) {
+    std::string text = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA.";
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    char symbol = 'l';
+    if (my_rank == 0) {
+        double freq = parFrequency(&symbol, text);
+        ASSERT_EQ((int)freq, 4);
+    }
+}
+
+TEST(Paralle_Operations_MPI, lower_upper_text) {
+    std::string text1 = "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA.",
+                text2 = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    char symbol = 'l';
+    if (my_rank == 0) {
+        double freq1 = parFrequency(&symbol, text1),
+               freq2 = parFrequency(&symbol, text2);
+        ASSERT_EQ((int)freq1, (int)freq2);
+    }
+}
+
+//----------------------------------------------------
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/chelebaev_a_symbol_frequency/symbol_frequency.cpp
@@ -0,0 +1,74 @@
+// Copyright 2021 Chelebaev Artem
+#include <mpi.h>
+#include <vector>
+#include <string>
+#include <random>
+#include <algorithm>
+#include "../../../modules/task_1/chelebaev_a_symbol_frequency/symbol_frequency.h"
+
+
+
+
+//--------------------------------------------------------------
+
+double parFrequency(char* symbol, std::string text)
+{
+    int my_rank, tasks, ccount = 0;    //variables for the rank of ongoing process, number of processes and symbol count respectively
+    double freq = 0;    //variable representing the frequency of a desired symbol
+    const int N = text.length();    //length of the incoming string
+
+    MPI_Status Status;
+    MPI_Comm_size(MPI_COMM_WORLD, &tasks);    //stating the number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);    //stating the rank of ongoing process
+
+
+    if (N < tasks) {    //in case there is less data than the number of processes, we terminate the program
+        throw "There is less data than the number of processes!";
+    }
+    int k = N / tasks;    //distribute the data between processes
+    int i1 = k * my_rank;    //state the first index
+    int i2 = k * (my_rank + 1);    //state second index
+    int count = 0;    //number of symbol entries in the string
+
+    if (my_rank == (tasks - 1)) {    //if current process is the last one, change the second index 
+                                    //so that the last process gets a bit more data than the others
+        i2 = N;
+    }
+    for (int i = i1; i < i2; i++) {    //look for symbol entries
+        if (text[i] == *symbol || text[i] == *symbol - 32)
+            count++;
+    }
+
+    if (my_rank == 0) {    //if current process is the first one, receive data from the other processes
+        ccount = count;
+        for (int i = 1; i < tasks; i++) {
+            MPI_Recv(&count, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &Status);
+            ccount += count;
+        }
+    }
+    else {    //else send the data to the first process
+        MPI_Send(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    if (my_rank == 0) {    //if current process is the first one, compute the frequency 
+        freq = ((double)ccount / (double)N) * 100;
+    }
+    return freq;
+}
+
+double seqFrequency(char* symbol, std::string text)
+{
+    int N = text.length();
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        if (text[i] == *symbol || text[i] == *symbol - 32) {
+            count++;
+        }
+    }
+    double freq = ((double)count / (double)N) * 100;
+    return freq;
+}
+
+//-----------------------------------------------------------------
+
+
