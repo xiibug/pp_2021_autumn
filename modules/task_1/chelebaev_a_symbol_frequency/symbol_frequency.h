4c6b7f0d3e731326e01f4a584c5dd3724f01c896
brimst0ne <artemchelebaev@gmail.com>
2021-11-17 22:07:03 +0300

    First MPI lab

--- modules/task_1/chelebaev_a_symbol_frequency/symbol_frequency.h
+++ modules/task_1/chelebaev_a_symbol_frequency/symbol_frequency.h
@@ -5,12 +5,6 @@
 #include <vector>
 #include <string>
 
-std::vector<int> getRandomVector(int  sz);
-int getParallelOperations(std::vector<int> global_vec,
-                          int count_size_vector, const std::string& ops);
-int getSequentialOperations(std::vector<int> vec, const std::string& ops);
-
-
 double parFrequency(char* symbol, std::string text);
 double seqFrequency(char* symbol, std::string text);
 #endif  // MODULES_TASK_1_NESTEROV_A_TEST_TASK_1_OPS_MPI_H_
