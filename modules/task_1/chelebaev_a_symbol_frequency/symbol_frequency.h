de6c0a1ecb865c42e7cfd202ff1de70f3bff5bcf
brimst0ne <artemchelebaev@gmail.com>
2021-11-17 22:13:11 +0300

    First MPI lab

--- modules/task_1/chelebaev_a_symbol_frequency/symbol_frequency.h
+++ modules/task_1/chelebaev_a_symbol_frequency/symbol_frequency.h
@@ -7,4 +7,4 @@
 
 double parFrequency(char* symbol, std::string text);
 double seqFrequency(char* symbol, std::string text);
-#endif  // MODULES_TASK_1_NESTEROV_A_TEST_TASK_1_OPS_MPI_H_
+#endif  // MODULES_TASK_1_CHELEBAEV_A_SYMBOL_FREQUENCY_SYMBOL_FREQUENCY_H_
