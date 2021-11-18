5bc0f31de2149c5b262a7fe9a5edb8a052b7c232
Max Borisov <arryowtf@gmail.com>
2021-11-18 17:04:59 +0300

    fixed tests && fixed code style

--- modules/task_1/borisov_m_scalar_vector_product/main.cpp
+++ modules/task_1/borisov_m_scalar_vector_product/main.cpp
@@ -1,6 +1,8 @@
-#include <gtest/gtest.h> 
-#include "./scalar_vector_product.h"
+// "Copyright [2021] <Borisov Maxim>"
+
 #include <mpi.h>
+#include <gtest/gtest.h>
+#include "./scalar_vector_product.h"
 #include <gtest-mpi-listener.hpp>
 
 TEST(parallel_mpi, test_scalar_std) {
@@ -25,8 +27,8 @@ TEST(parallel_mpi, test_scalar_std) {
 
 
 TEST(parallel_mpi, test_scalar_mine) {
-	int proc_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    int proc_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
     std::vector<int> vector_a;
     std::vector<int> vector_b;
     if (proc_rank == 0) {
@@ -66,8 +68,8 @@ TEST(parallel_mpi, test_scalar_std_2) {
 
 
 TEST(parallel_mpi, test_scalar_mine_1) {
-	int proc_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    int proc_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
     std::vector<int> vector_a;
     std::vector<int> vector_b;
     if (proc_rank == 0) {
@@ -102,22 +104,20 @@ TEST(parallel_mpi, test_scalar_std_3) {
         int seq_result = sequential_product_std(vector_a, vector_b);
         ASSERT_EQ(par_result, seq_result);
     }
-
 }
 
 
 int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
 
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-
-	return RUN_ALL_TESTS();
-
+    return RUN_ALL_TESTS();
 }
 
--- modules/task_1/borisov_m_scalar_vector_product/scalar_vector_product.cpp
+++ modules/task_1/borisov_m_scalar_vector_product/scalar_vector_product.cpp
@@ -1,56 +1,56 @@
-#include "scalar_vector_product.h"
+// "Copyright [2021] <Borisov Maxim>"
+
+#include "../../../modules/task_1/borisov_m_scalar_vector_product/scalar_vector_product.h"
 #include <mpi.h>
 #include <random>
 #include <numeric>
 
-int sequential_product(const std::vector<int>& vector_a, const std::vector<int>& vector_b) {
-	if (vector_a.size() != vector_b.size())
-		return -1;
-	int result = 0;
-	for (std::vector<int>::size_type i = 0; i < vector_a.size(); i++) {
-		result += vector_a[i] * vector_b[i];
-	}
-	return result;
+int sequential_product(const std::vector<int>& vector_a,
+        const std::vector<int>& vector_b) {
+    if (vector_a.size() != vector_b.size())
+        return -1;
+    int result = 0;
+    for (std::vector<int>::size_type i = 0; i < vector_a.size(); i++) {
+        result += vector_a[i] * vector_b[i];
+    }
+    return result;
 }
 
-int sequential_product_std(const std::vector<int>& vector_a, const std::vector<int>&  vector_b) {
-	int result = std::inner_product(vector_a.begin(), vector_a.end(),vector_b.begin(), 0);
-	return result;
+int sequential_product_std(const std::vector<int>& vector_a,
+        const std::vector<int>&  vector_b) {
+    int result = std::inner_product(vector_a.begin(),
+        vector_a.end(), vector_b.begin(), 0);
+    return result;
 }
 
 std::vector<int> get_random_vector(int size) {
-	std::vector<int> vector(size);
-	std::random_device r_device;
-	std::mt19937 rand(r_device());
-	for(int i = 0; i < size; i++) {
-		vector[i] = rand() % 100;
-	}
-	return vector;
-
+    std::vector<int> vector(size);
+    std::random_device r_device;
+    std::mt19937 rand_r(r_device());
+    for (int i = 0; i < size; i++) {
+        vector[i] = rand_r() % 100;
+    }
+    return vector;
 }
 
 int parallel_product(std::vector<int> vector_a, std::vector<int> vector_b) {
-	if (vector_a.size() != vector_b.size())
-		return -1;
-	int size = vector_a.size();
-	int proc_count = 0;
-	int proc_rank = 0;
-	MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
-	MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    if (vector_a.size() != vector_b.size())
+        return -1;
+    int size = vector_a.size();
+    int proc_count = 0;
+    int proc_rank = 0;
+    int result = 0;
+    int local_result = 0;
+    MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    MPI_Bcast(vector_a.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(vector_b.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
 
-	int result = 0;
-	int local_size = size / proc_count;
+    for (int i = proc_rank * size / proc_count;
+        i < (proc_rank + 1) * size / proc_count; i++) {
+        local_result += vector_a[i] * vector_b[i];
+    }
 
-	std::vector<int> local_a(local_size);
-	std::vector<int> local_b(local_size);
-
-	MPI_Scatter(vector_a.data(), local_size, MPI_INT, local_a.data(), local_size, MPI_INT, 0, MPI_COMM_WORLD);
-	MPI_Scatter(vector_b.data(), local_size, MPI_INT, local_b.data(), local_size, MPI_INT, 0, MPI_COMM_WORLD);
-
-	int local_result = sequential_product_std(local_a, local_b);
-
-	MPI_Reduce(&local_result, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-
-	return result;
+    MPI_Reduce(&local_result, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    return result;
 }
