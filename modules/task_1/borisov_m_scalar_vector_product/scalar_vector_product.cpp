fb2298c7507e7fbf48eae2da4a148632b8ef79ca
Max Borisov <arryowtf@gmail.com>
2021-11-19 12:39:33 +0300

    fixed v5

--- modules/task_1/borisov_m_scalar_vector_product/scalar_vector_product.cpp
+++ modules/task_1/borisov_m_scalar_vector_product/scalar_vector_product.cpp
@@ -4,6 +4,7 @@
 #include <mpi.h>
 #include <random>
 #include <numeric>
+#include <iterator>
 
 int sequential_product(const std::vector<int>& vector_a,
         const std::vector<int>& vector_b) {
@@ -11,6 +12,14 @@ int sequential_product(const std::vector<int>& vector_a,
         return -1;
     int result = 0;
     for (std::vector<int>::size_type i = 0; i < vector_a.size(); i++) {
+        result += vector_a[i] * vector_b[i];
+    }
+    return result;
+}
+
+int sequential_product(int* vector_a, int* vector_b, int size) {
+    int result = 0;
+    for (std::vector<int>::size_type i = 0; i < size; i++) {
         result += vector_a[i] * vector_b[i];
     }
     return result;
@@ -33,24 +42,41 @@ std::vector<int> get_random_vector(int size) {
     return vector;
 }
 
+
 int parallel_product(std::vector<int> vector_a, std::vector<int> vector_b) {
     if (vector_a.size() != vector_b.size())
         return -1;
     int size = vector_a.size();
     int proc_count = 0;
     int proc_rank = 0;
+    int dummy = 0;
     int result = 0;
     int local_result = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
-    MPI_Bcast(vector_a.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(vector_b.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    int local_size = size / proc_count; 
+    std::vector<int> loc_a(local_size);
+    std::vector<int> loc_b(local_size);
+  if (proc_rank == 0) {
+       std::cout << "SIZE: " << proc_count << std::endl;
+       if (size % proc_count != 0) {
+           for(int i = size - size % proc_count; i < size; i++) {
+               loc_a.push_back(vector_a[i]);
+               loc_b.push_back(vector_b[i]);
+           }
+       }
+   }
 
-    for (int i = proc_rank * size / proc_count;
-        i < (proc_rank + 1) * size / proc_count; i++) {
-        local_result += vector_a[i] * vector_b[i];
-    }
+   MPI_Scatter(vector_a.data(), local_size, MPI_INT, loc_a.data(), local_size, 
+           MPI_INT, 0, MPI_COMM_WORLD);
+   MPI_Scatter(vector_b.data(), local_size, MPI_INT, loc_b.data(), local_size, 
+           MPI_INT, 0, MPI_COMM_WORLD);
+
+    local_result = sequential_product_std(loc_a, loc_b);
 
     MPI_Reduce(&local_result, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
     return result;
 }
