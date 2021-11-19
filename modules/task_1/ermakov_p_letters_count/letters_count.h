939bb1836f26f631ec3c40569052accfac851071
Leyzeld <leyzeld@gmail.com>
2021-11-19 20:20:52 +0300

    style-fix2

--- modules/task_1/ermakov_p_letters_count/letters_count.h
+++ modules/task_1/ermakov_p_letters_count/letters_count.h
@@ -1,9 +1,9 @@
 // Copyright 2021 Ermakov Pavel
-#include <string>
 #include <mpi.h>
-#include <numeric>
 #include <gtest/gtest.h>
 #include <gtest-mpi-listener.hpp>
+#include <string>
+#include <numeric>
 
 #ifndef MODULES_TASK_1_ERMAKOV_P_LETTERS_COUNT_LETTERS_COUNT_H_
 #define MODULES_TASK_1_ERMAKOV_P_LETTERS_COUNT_LETTERS_COUNT_H_
@@ -13,4 +13,4 @@ int letters_count_seq(std::string str);
 int letters_count_par(std::string str);
 
 
-#endif  // MODULES_TASK_1_ERMAKOV_P_LETTERS_COUNT_LETTERS_COUNT_
+#endif  // MODULES_TASK_1_ERMAKOV_P_LETTERS_COUNT_LETTERS_COUNT_H_
