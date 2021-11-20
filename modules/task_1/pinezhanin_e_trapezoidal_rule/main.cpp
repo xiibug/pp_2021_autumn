3de210f58ed9bcda75ff1543ebefc44563fc9a28
EvgenyPinezhanin <pinezhanin@yandex.ru>
2021-11-21 01:14:48 +0300

    added test layouts

--- modules/task_1/pinezhanin_e_trapezoidal_rule/main.cpp
+++ modules/task_1/pinezhanin_e_trapezoidal_rule/main.cpp
@@ -3,6 +3,30 @@
 #include "./trapezoidal_rule.h"
 #include <gtest-mpi-listener.hpp>
 
+double f1(double x) {
+    return x * x;
+}
+
+TEST(Trapezoidal_rule_test, one) {
+
+}
+
+TEST(Trapezoidal_rule_test, two) {
+    
+}
+
+TEST(Trapezoidal_rule_test, three) {
+    
+}
+
+TEST(Trapezoidal_rule_test, four) {
+    
+}
+
+TEST(Trapezoidal_rule_test, five) {
+    
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
