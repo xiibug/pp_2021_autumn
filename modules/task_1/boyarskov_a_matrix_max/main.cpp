9bca4dded79acd354e48f6aa503cd12dd4f7e381
ABoyarskov <anboyarskov@yandex.ru>
2021-11-20 23:49:54 +0300

    fix tests

--- modules/task_1/boyarskov_a_matrix_max/main.cpp
+++ modules/task_1/boyarskov_a_matrix_max/main.cpp
@@ -21,6 +21,78 @@ TEST(Parallel_Max_MPI, Size_2x3) {
     }
 }
 
+TEST(Parallel_Max_MPI, Size_5x5) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int* matrix = nullptr;
+    const int n = 5, m = 5;
+
+    if (rank == 0) {
+        matrix = getRandomMatrix(n, m);
+    }
+
+    int global_max = getParallelMax(matrix, n, m);
+
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix, n, m);
+        ASSERT_EQ(reference_max, global_max);
+    }
+}
+
+TEST(Parallel_Max_MPI, Size_10x15) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int* matrix = nullptr;
+    const int n = 10, m = 15;
+
+    if (rank == 0) {
+        matrix = getRandomMatrix(n, m);
+    }
+
+    int global_max = getParallelMax(matrix, n, m);
+
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix, n, m);
+        ASSERT_EQ(reference_max, global_max);
+    }
+}
+
+TEST(Parallel_Max_MPI, Size_20x30) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int* matrix = nullptr;
+    const int n = 2, m = 3;
+
+    if (rank == 0) {
+        matrix = getRandomMatrix(n, m);
+    }
+
+    int global_max = getParallelMax(matrix, n, m);
+
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix, n, m);
+        ASSERT_EQ(reference_max, global_max);
+    }
+}
+
+TEST(Parallel_Max_MPI, Size_50x50) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int* matrix = nullptr;
+    const int n = 2, m = 3;
+
+    if (rank == 0) {
+        matrix = getRandomMatrix(n, m);
+    }
+
+    int global_max = getParallelMax(matrix, n, m);
+
+    if (rank == 0) {
+        int reference_max = getSequentialMax(matrix, n, m);
+        ASSERT_EQ(reference_max, global_max);
+    }
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
