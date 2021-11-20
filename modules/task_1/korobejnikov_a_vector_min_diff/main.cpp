018b5817c334f72ad461625b1fa4c7f7adae9e18
KorobejnikovAP <armok2001@mail.ru>
2021-11-20 05:40:39 +0300

    fix style again

--- modules/task_1/korobejnikov_a_vector_min_diff/main.cpp
+++ modules/task_1/korobejnikov_a_vector_min_diff/main.cpp
@@ -53,51 +53,51 @@ TEST(Parallel_Operations_MPI, Test_Min3) {
 }
 
 TEST(Parallel_Operations_MPI, Test_Min4) {
-	int rank;
-
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 120;
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
-	std::pair<int, int> globalMin = parallelCount(global_vec, count_size_vector);
-	if (rank == 0) {
-		std::pair<int, int> reference_min = sequentialCount(global_vec, count_size_vector);
-		ASSERT_EQ(globalMin, reference_min);
-	}
+    int rank;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    std::pair<int, int> globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        std::pair<int, int> reference_min = sequentialCount(global_vec, count_size_vector);
+        ASSERT_EQ(globalMin, reference_min);
+    }
 }
 
 TEST(Parallel_Operations_MPI, Test_Min5) {
-	int rank;
-
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 120;
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
-	std::pair<int, int> globalMin = parallelCount(global_vec, count_size_vector);
-	if (rank == 0) {
-		std::pair<int, int> reference_min = sequentialCount(global_vec, count_size_vector);
-		ASSERT_EQ(globalMin, reference_min);
-	}
+    int rank;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    std::pair<int, int> globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        std::pair<int, int> reference_min = sequentialCount(global_vec, count_size_vector);
+        ASSERT_EQ(globalMin, reference_min);
+    }
 }
 
 TEST(Parallel_Operations_MPI, Test_Min6) {
-	int rank;
-
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::vector<int> global_vec;
-	const int count_size_vector = 120;
-	if (rank == 0) {
-		global_vec = getRandomVector(count_size_vector);
-	}
-	std::pair<int, int> globalMin = parallelCount(global_vec, count_size_vector);
-	if (rank == 0) {
-		std::pair<int, int> reference_min = sequentialCount(global_vec, count_size_vector);
-		ASSERT_EQ(globalMin, reference_min);
-	}
+    int rank;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 120;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    std::pair<int, int> globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        std::pair<int, int> reference_min = sequentialCount(global_vec, count_size_vector);
+        ASSERT_EQ(globalMin, reference_min);
+    }
 }
 
 
