63cbd1f8ab078cd1247a95fc75372f42bb60e890
Ilya Kamenev <ilia.kamenevv@yandex.ru>
2021-11-22 19:50:01 +0300

    fixx

--- modules/task_1/kamenev_i_counting_letters_number/counting_letters_number.cpp
+++ modules/task_1/kamenev_i_counting_letters_number/counting_letters_number.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 Kamenev Ilya
-#include "../../../modules/task_1/kamenev_i_counting_letters_number/counting_letters_number.h"
+#include "counting_letters_number.h"
 
 
 std::string CreateRandomStr(size_t size) {
@@ -35,26 +35,21 @@ int CountingLettersParallel(const std::string& str) {
     int rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int delta = str.size() / size;
-
+    const int delta = str.size() / size;
     if (rank == 0) {
         for (int proc = 1; proc < size; proc++) {
             MPI_Send(str.data() + proc * delta, delta, MPI_CHAR, proc,
                 0, MPI_COMM_WORLD);
         }
     }
-
     std::string partial_str;
     if (rank == 0) {
         partial_str = std::string(str.begin(), str.begin() + delta);
     } else {
         MPI_Status status;
-        MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        MPI_Get_count(&status, MPI_CHAR, &delta);
         partial_str.resize(delta);
         MPI_Recv(&partial_str[0], delta, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
     }
-
     int global_letters_count = 0;
     int partial_letters_count = CountingLettersSequential(partial_str);
     MPI_Reduce(&partial_letters_count, &global_letters_count, 1,
--- modules/task_1/kamenev_i_counting_letters_number/main.cpp
+++ modules/task_1/kamenev_i_counting_letters_number/main.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 Kamenev Ilya
-#include "../../../modules/task_1/kamenev_i_counting_letters_number/counting_letters_number.h"
+#include "counting_letters_number.h"
 
 TEST(Parallel_Operations_MPI, Test_Empty_Random_Str) {
     int rank;
@@ -8,7 +8,6 @@ TEST(Parallel_Operations_MPI, Test_Empty_Random_Str) {
     const int str_size = 0;
     str = CreateRandomStr(str_size);
     int par_result = CountingLettersParallel(str);
-
     if (rank == 0) {
         int expected_result = 0;
         ASSERT_EQ(expected_result, par_result);
@@ -22,22 +21,20 @@ TEST(Parallel_Operations_MPI, Test_Only_Letters_100) {
     const int str_size = 100;
     str = CreateOnlyLettersStr(str_size);
     int par_result = CountingLettersParallel(str);
-
     if (rank == 0) {
         int expected_result = 100;
         ASSERT_EQ(expected_result, par_result);
     }
 }
 
-TEST(Parallel_Operations_MPI, Test_20_Letters_Str) {
+TEST(Parallel_Operations_MPI, Test_100_Letters_Str) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     std::string str;
-    str = "abcdefghijABCDEFGHIJ";
+    str = CreateOnlyLettersStr(100);
     int par_result = CountingLettersParallel(str);
-
     if (rank == 0) {
-        int expected_result = 20;
+        int expected_result = 100;
         ASSERT_EQ(expected_result, par_result);
     }
 }
@@ -51,7 +48,6 @@ TEST(Parallel_Operations_MPI, Test_Empty_Str_Parallel_Equals_Sequential) {
     int par_result, seq_result;
     par_result = CountingLettersParallel(str);
     seq_result = CountingLettersSequential(str);
-
     if (rank == 0) {
         ASSERT_EQ(seq_result, par_result);
     }
@@ -64,7 +60,6 @@ TEST(Parallel_Operations_MPI, Test_No_Letters_Str) {
     str = "$%##4235!8*15%79)-7$}=";
     int result;
     result = CountingLettersParallel(str);
-
     if (rank == 0) {
         int expected_result = 0;
         ASSERT_EQ(expected_result, result);
@@ -74,14 +69,12 @@ TEST(Parallel_Operations_MPI, Test_No_Letters_Str) {
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
-
     ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
     ::testing::TestEventListeners& listeners =
         ::testing::UnitTest::GetInstance()->listeners();
-
     listeners.Release(listeners.default_result_printer());
     listeners.Release(listeners.default_xml_generator());
-
     listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
     return RUN_ALL_TESTS();
+    MPI_Finalize();
 }
