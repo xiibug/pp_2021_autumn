d0dcc24caed50271af4d28a4ce1c72d4eba31366
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-11-22 21:25:25 +0300

    All realisation

---
+++ modules/task_1/brazhnik_d_vector_find_min/find_min_element_vector.h
@@ -0,0 +1,14 @@
+// Copyright 2021 Brazhnik Dmitry
+#ifndef MODULES_TASK_1_BRAZHNIK_D_FIND_MIN_ELEMENT_VECTOR_H_
+#define MODULES_TASK_1_BRAZHNIK_D_FIND_MIN_ELEMENT_VECTOR_H_
+
+#include <string>
+#include <vector>
+
+std::vector<int> fillRandomValToVector(const int size);
+void viewVector(const std::vector<int>& matrix);
+
+int defaultFindingMinVal(const std::vector<int>& vector);
+int parallelFindingMinVal(const std::vector<int>& vector, const int size);
+
+#endif  // MODULES_TASK_1_BRAZHNIK_D_FIND_MIN_ELEMENT_VECTOR_H_
