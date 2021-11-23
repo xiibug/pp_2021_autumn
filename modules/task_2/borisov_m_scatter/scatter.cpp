7760ebd15b0d68133d7537ea5b56d9ef18ba4440
Max Borisov <arryowtf@gmail.com>
2021-11-23 23:16:12 +0300

    fixed issues

--- modules/task_2/borisov_m_scatter/scatter.cpp
+++ modules/task_2/borisov_m_scatter/scatter.cpp
@@ -9,8 +9,8 @@ int my_scatter(void *sendbuf, int sendcount, MPI_Datatype sendtype,
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
 
-    int send_type_size;
-    int recv_type_size;
+    int sendtype_size; 
+    int recvtype_size;
 
     if (sendcount != recvcount)
         return MPI_ERR_COUNT;
@@ -18,18 +18,19 @@ int my_scatter(void *sendbuf, int sendcount, MPI_Datatype sendtype,
         return MPI_ERR_COUNT;
     if (root < 0)
         return MPI_ERR_COUNT;
-    if (MPI_Type_size(sendtype, &send_type_size) == MPI_ERR_TYPE)
+    // MPI_Type_size set sendtype_size to sizeof(sendtype)
+    if (MPI_Type_size(sendtype, &sendtype_size) == MPI_ERR_TYPE)
         return MPI_ERR_TYPE;
-    if (MPI_Type_size(recvtype, &recv_type_size) == MPI_ERR_TYPE)
+    if (MPI_Type_size(recvtype, &recvtype_size) == MPI_ERR_TYPE)
         return MPI_ERR_TYPE;
 
     if (proc_rank == root) {
         memcpy(recvbuf, reinterpret_cast<char*>(sendbuf) +
-                proc_rank * sendcount * send_type_size,
-                sendcount * send_type_size);
+                proc_rank * sendcount * sendtype_size,
+                sendcount * sendtype_size);
         for (int i = 0; i < proc_count; i++) {
             MPI_Send(reinterpret_cast<char*>(sendbuf) +
-                    i * sendcount * send_type_size,
+                    i * sendcount * sendtype_size,
                     sendcount, sendtype, i, 0, comm);
         }
     } else {
