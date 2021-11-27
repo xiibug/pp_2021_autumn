d3f69f0ba37ad287dc9a47505c08737377e6b1cb
Kcenia <Kcenia4010@gmail.com>
2021-11-27 12:40:49 +0300

    Revert "no fix"
    This reverts commit b722823b3ef59c3566a276a3a7b44d8e624bfafc.

--- modules/task_1/zaitseva_k_integral_rectangle_method/main.cpp
+++ modules/task_1/zaitseva_k_integral_rectangle_method/main.cpp
@@ -1,88 +1,85 @@
 // Copyright 2021 Zaitseva Ksenia
 #include <gtest/gtest.h>
-
+#include <mpi.h>
+#include <cmath>
+#include <iostream>
+#include "RectangleMethod.h"
 #include <gtest-mpi-listener.hpp>
 
-#include "RectangleMethod.h"
-
-TEST(DiningPhilosophers, one_run) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int runs = 1;
-
-  DiningPholisophers(rank, size, runs);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  ASSERT_NO_THROW();
+TEST(Parallel_MPI, Test_Sin) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double a = 0.0, b = 5.5;
+    int n = 1000;
+    double res_seq, res_par;
+    res_par = IntegralParallel(a, b, n, 0);
+    if (rank == 0) {
+        res_seq = IntegralSequential(a, b, n, 0);
+        EXPECT_NEAR(res_seq, res_par, std::numeric_limits<double>::epsilon() * std::max(res_par, res_seq) * 16 * 1000);
+    }
 }
 
-TEST(DiningPhilosophers, two_run) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int runs = 2;
-
-  DiningPholisophers(rank, size, runs);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  ASSERT_NO_THROW();
+TEST(Parallel_MPI, Test_Polinom1) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double a = 0.0, b = 5.5;
+    int n = 1000;
+    double res_seq, res_par;
+    res_par = IntegralParallel(a, b, n, 1);
+    if (rank == 0) {
+        res_seq = IntegralSequential(a, b, n, 1);
+        EXPECT_NEAR(res_seq, res_par, std::numeric_limits<double>::epsilon() * std::max(res_par, res_seq) * 16 * 1000);
+    }
 }
 
-TEST(DiningPhilosophers, five_run) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int runs = 5;
-
-  DiningPholisophers(rank, size, runs);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  ASSERT_NO_THROW();
+TEST(Parallel_MPI, Test_Polinom2) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double a = 0.0, b = 5.5;
+    int n = 1000;
+    double res_seq, res_par;
+    res_par = IntegralParallel(a, b, n, 2);
+    if (rank == 0) {
+        res_seq = IntegralSequential(a, b, n, 2);
+        EXPECT_NEAR(res_seq, res_par, std::numeric_limits<double>::epsilon() * std::max(res_par, res_seq) * 16 * 1000);
+    }
 }
 
-TEST(DiningPhilosophers, ten_run) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int runs = 10;
-
-  DiningPholisophers(rank, size, runs);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  ASSERT_NO_THROW();
+TEST(Parallel_MPI, Test_Polinom3) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double a = 0.0, b = 5.5;
+    int n = 1000;
+    double res_seq, res_par;
+    res_par = IntegralParallel(a, b, n, 3);
+    if (rank == 0) {
+        res_seq = IntegralSequential(a, b, n, 3);
+        EXPECT_NEAR(res_seq, res_par, std::numeric_limits<double>::epsilon() * std::max(res_par, res_seq) * 16 * 1000);
+    }
 }
 
-TEST(DiningPhilosophers, ten_fifty) {
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  int runs = 25;
-
-  DiningPholisophers(rank, size, runs);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  ASSERT_NO_THROW();
+TEST(Parallel_MPI, Test_Cos) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double a = 0.0, b = 5.5;
+    int n = 1000;
+    double res_seq, res_par;
+    res_par = IntegralParallel(a, b, n, 4);
+    if (rank == 0) {
+        res_seq = IntegralSequential(a, b, n, 4);
+        EXPECT_NEAR(res_seq, res_par, std::numeric_limits<double>::epsilon() * 16 * 1000);
+    }
 }
 
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  MPI_Init(&argc, &argv);
-
-  std::srand(std::time(nullptr));
-
-  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-  ::testing::TestEventListeners& listeners =
-      ::testing::UnitTest::GetInstance()->listeners();
-
-  listeners.Release(listeners.default_result_printer());
-  listeners.Release(listeners.default_xml_generator());
-
-  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+    MPI_Finalize();
 }
