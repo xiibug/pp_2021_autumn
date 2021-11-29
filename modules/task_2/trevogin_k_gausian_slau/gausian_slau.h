03f2e2feafcf01c216a6fe0b4a6595ca90e3808c
kirill123kk <64713868+kirill123kk@users.noreply.github.com>
2021-11-30 00:31:24 +0300

    fix_stil

--- modules/task_2/trevogin_k_gausian_slau/gausian_slau.h
+++ modules/task_2/trevogin_k_gausian_slau/gausian_slau.h
@@ -9,12 +9,13 @@
 #ifndef MODULES_TASK_2_TREVOGIN_K_GAUSIAN_SLAU_GAUSIAN_SLAU_H_
 #define MODULES_TASK_2_TREVOGIN_K_GAUSIAN_SLAU_GAUSIAN_SLAU_H_
 void out_mat(std::vector<double> mat, std::vector<double> vec);
-std::vector<double> triangulation(std::vector<double> mat, int line, std::vector<double>& vec);
-std::vector<double> getRandomVector(std::vector<double>& mat, int line);
+std::vector<double> triangulation(std::vector<double> mat, int line, const std::vector<double>& vec);
+std::vector<double> getRandomVector(const std::vector<double>& mat, int line);
 std::vector<double> gaus_metod(std::vector<double> mat, std::vector<double> vec);
 std::vector<double> res(std::vector<double> mat, std::vector<double> vec);
-void DataDistribution(std::vector<double>& mat, std::vector<double>& vec, int& line, std::vector<double>& mat1);
+void DataDistribution(const std::vector<double>& mat, const std::vector<double>& vec, const int& line, const std::vector<double>& mat1);
 std::vector<double> gaus_metod_parall(std::vector<double> mat, std::vector<double> vec);
-std::vector<double> triangulation_parall(std::vector<double> mat, int line, std::vector<double>& vec);
+std::vector<double> triangulation_parall(std::vector<double> mat, int line, const std::vector<double>& vec);
 std::vector<double> res_parall(std::vector<double> mat, int line, std::vector<double> vec, std::vector<double> vec1);
+
 #endif // MODULES_TASK_2_TREVOGIN_K_GAUSIAN_SLAU_GAUSIAN_SLAU_H_
