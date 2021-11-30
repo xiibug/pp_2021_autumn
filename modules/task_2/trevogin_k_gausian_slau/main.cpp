0fbd8eac4fbffa45f4eae00bbd22de92357313e3
kirill123kk <64713868+kirill123kk@users.noreply.github.com>
2021-11-30 15:25:50 +0300

    fix_dmc_2

--- modules/task_2/trevogin_k_gausian_slau/main.cpp
+++ modules/task_2/trevogin_k_gausian_slau/main.cpp
@@ -80,10 +80,13 @@ TEST(Parallel_Operations_MPI, Test_Gaus_metod_5) {
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
+
     ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
     ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+
     listeners.Release(listeners.default_result_printer());
     listeners.Release(listeners.default_xml_generator());
+
     listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
     return RUN_ALL_TESTS();
 }
