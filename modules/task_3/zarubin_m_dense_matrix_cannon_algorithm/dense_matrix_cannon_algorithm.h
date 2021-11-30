6f922fcd89790efc1e1c057459369a12271edf18
MikhailZarubin <mikhail.zarubin.itmm@mail.ru>
2021-11-30 16:49:34 +0300

    Added task 3

---
+++ modules/task_3/zarubin_m_dense_matrix_cannon_algorithm/dense_matrix_cannon_algorithm.h
@@ -0,0 +1,25 @@
+// Copyright 2021 Zarubin Mikhail
+#ifndef MODULES_TASK_3_ZARUBIN_M_DENSE_MATRIX_CANNON_ALGORITHM_DENSE_MATRIX_CANNON_ALGORITHM_H
+#define MODULES_TASK_3_ZARUBIN_M_DENSE_MATRIX_CANNON_ALGORITHM_DENSE_MATRIX_CANNON_ALGORITHM_H
+#include <vector>
+
+void generateRandomMatrix(std::vector<double>* matrix,
+    std::vector<double>::size_type size,
+    int left_dorder, int right_border);
+void getSequentialOperations(const std::vector<double>& first_matrix,
+    const std::vector<double>& second_matrix,
+    std::vector<double>* answer,
+    std::vector<double>::size_type size);
+std::vector<double> getParallelOperations(const std::vector<double>& first_matrix,
+    const std::vector<double>& second_matrix,
+    std::vector<double>::size_type size);
+int calculateActualSize(int old_size,
+    std::vector<double>::size_type matrix_size);
+void getPartMatrix(const std::vector<double>& orig_matrix,
+    std::vector<double>::size_type matrix_size,
+    std::vector<double>* part_matrix,
+    std::vector<double>::size_type local_size,
+    std::vector<double>::size_type count_parts,
+    std::vector<double>::size_type start_offset);
+
+#endif  // MODULES_TASK_3_ZARUBIN_M_DENSE_MATRIX_CANNON_ALGORITHM_DENSE_MATRIX_CANNON_ALGORITHM_Hs
