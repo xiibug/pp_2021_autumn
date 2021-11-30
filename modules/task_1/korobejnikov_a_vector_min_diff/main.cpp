3d121cdef118f0f1a21883d0a52ddce6f27dcf93
KorobejnikovAP <armok2001@mail.ru>
2021-11-30 22:27:06 +0300

    fix

--- modules/task_1/korobejnikov_a_vector_min_diff/main.cpp
+++ modules/task_1/korobejnikov_a_vector_min_diff/main.cpp
@@ -94,6 +94,66 @@ TEST(Parallel_Operations_MPI, Test_even_count_3) {
     }
 }
 
+TEST(Parallel_Operations_MPI, Test_7) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 9;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    int globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        int reference_min = sequentialCount(global_vec);
+        ASSERT_EQ(globalMin, reference_min);
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_8) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 11;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    int globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        int reference_min = sequentialCount(global_vec);
+        ASSERT_EQ(globalMin, reference_min);
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_9) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 10;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    int globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        int reference_min = sequentialCount(global_vec);
+        ASSERT_EQ(globalMin, reference_min);
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_10) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 24;
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+    int globalMin = parallelCount(global_vec, count_size_vector);
+    if (rank == 0) {
+        int reference_min = sequentialCount(global_vec);
+        ASSERT_EQ(globalMin, reference_min);
+    }
+}
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
