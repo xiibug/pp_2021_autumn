5f4261e3549acd6d2eae738820d6c03c9127bb91
JavierGalindoToledo <javier.galindo.toledo13@gmail.com>
2021-12-02 23:59:12 +0300

    version 1

---
+++ modules/task_1/galindo_rectangular_method/galindo_rectangular_method.cpp
@@ -0,0 +1,36 @@
+#include <mpi.h>
+#include <cmath>
+#include "galindo_rectangular_method.h"
+
+double def_int(std::function<double(double)> funct, double a, double b, int count) {
+	double sfc = 0;
+	double h = (b - a) / count;
+	for (int i = 0; i < count; i++) {
+		sfc += funct(a + i * h);
+	}
+	double res = h * sfc;
+	return res;
+}
+
+double parallel_int(std::function<double(double)> funct, double a, double b, int count) {
+	int rank, size;
+	double sfc = (b - a) / count;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &rank);
+	int l_count = count / size;
+	double res = 0, l_res = 0,
+		l_a = a + rank * l_count * sfc,
+		l_b = l_a + l_count * sfc;
+	if (rank != size - 1) {
+		l_res = def_int(funct, l_a, l_b, l_count);
+	}
+	else {
+		l_res = def_int(funct, l_a, b, count - l_count * (size - 1));
+	}
+	MPI_Reduce(&l_res, &res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+}
+double func(double x) {
+	return(2 * x * x * x - 7 * x + 4);
+}
+
+
---
+++ modules/task_1/galindo_rectangular_method/galindo_rectangular_method.h
@@ -0,0 +1,12 @@
+#ifndef MODULES_TASK_1_GALINDO_RECTANGULAR_METHOD_RECTANGULAR_METHOD_H_
+#define MODULES_TASK_1_GALINDO_RECTANGULAR_METHOD_RECTANGULAR_METHOD_H_
+#include <functional>
+#include <mpi.h>
+
+double def_int(std::function<double(double)> funct, double a, double b, int count);
+double parallel_int(std::function<double(double)> funct, double a, double b, int count);
+
+double func(double x);
+
+#endif // !MODULES_TASK_1_GALINDO_RECTANGULAR_METHOD_RECTANGULAR_METHOD_H_
+
