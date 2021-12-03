0c3793c03b79556023fa37d313e32162cb485bf4
EvgenyPinezhanin <pinezhanin@yandex.ru>
2021-12-03 19:14:57 +0300

    add dynamic array

--- modules/task_2/pinezhanin_e_bubble_sort_odd_even/bubble_sort_odd_even.cpp
+++ modules/task_2/pinezhanin_e_bubble_sort_odd_even/bubble_sort_odd_even.cpp
@@ -20,9 +20,11 @@ std::vector<int> BubbleSortOddEvenParallel(std::vector<int> vec) {
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int sendCounts[size], displs[size], n;
+    int* sendCounts = new int[size];
+    int* displs = new int[size];
+    int n;
 
-    n = vec.size();
+    n = (int)vec.size();
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     for (int i = 0; i < size; i++) {
@@ -36,7 +38,7 @@ std::vector<int> BubbleSortOddEvenParallel(std::vector<int> vec) {
         displs[i] = displs[i - 1] + sendCounts[i - 1];
     }
 
-    int part_vec[sendCounts[rank]];
+    int *part_vec = new int[sendCounts[rank]];
     int odd, a;
     MPI_Scatterv(vec.data(), sendCounts, displs, MPI_INT, part_vec, sendCounts[rank], MPI_INT, 0, MPI_COMM_WORLD);
     for (int i = 0; i < n; i++) {
@@ -65,11 +67,14 @@ std::vector<int> BubbleSortOddEvenParallel(std::vector<int> vec) {
         MPI_Barrier(MPI_COMM_WORLD);
     }
     MPI_Gatherv(part_vec, sendCounts[rank], MPI_INT, vec.data(), sendCounts, displs, MPI_INT, 0, MPI_COMM_WORLD);
+    delete[] sendCounts;
+    delete[] displs;
+    delete[] part_vec;
     return vec;
 }
 
 std::vector<int> BubbleSortOddEvenSequential(std::vector<int> vec) {
-    int n = vec.size();
+    int n = (int)vec.size();
     for (int i = 0; i < n; i++) {
         if (i % 2) {
             for (int j = 1; j < n - 1; j += 2) {
