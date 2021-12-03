f2c293b46efcea5849345124f724af97796ee804
JavierGalindoToledo <javier.galindo.toledo13@gmail.com>
2021-12-03 15:02:08 +0300

    version 2

--- modules/task_1/galindo_rectangular_method/galindo_rectangular_method.h
+++ modules/task_1/galindo_rectangular_method/galindo_rectangular_method.h
@@ -1,7 +1,6 @@
 #ifndef MODULES_TASK_1_GALINDO_RECTANGULAR_METHOD_RECTANGULAR_METHOD_H_
 #define MODULES_TASK_1_GALINDO_RECTANGULAR_METHOD_RECTANGULAR_METHOD_H_
 #include <functional>
-#include <mpi.h>
 
 double def_int(std::function<double(double)> funct, double a, double b, int count);
 double parallel_int(std::function<double(double)> funct, double a, double b, int count);
