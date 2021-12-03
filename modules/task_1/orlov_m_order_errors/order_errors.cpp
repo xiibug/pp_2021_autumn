30eb0d613a50605d8073abbe05b85be4925f63c1
Орлов Максим Денисович <orlov.maxim2001@gmail.com>
2021-12-03 12:59:19 +0300

    Removed invalid scatterv call

--- modules/task_1/orlov_m_order_errors/order_errors.cpp
+++ modules/task_1/orlov_m_order_errors/order_errors.cpp
@@ -11,14 +11,8 @@ int orderErrors(std::vector<int> v) {
     int n_ = static_cast<int>(v.size()) - 1;
     int* displs;
     int* scounts;
-    int* arr = new int[static_cast<int>(v.size())];
     MPI_Comm_size(MPI_COMM_WORLD, &procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    if (rank == 0) {
-        for (int i = 0; i < static_cast<int>(v.size()); i++) {
-            arr[i] = v[i];
-        }
-    }
     rem = n_ % procs;
     pairsPerProc = n_ / procs;
     displs = new int[procs];
@@ -36,15 +30,13 @@ int orderErrors(std::vector<int> v) {
     }
     int* v_ = new int[scounts[rank]];
     //  MPI_Scatterv(v.data(), scounts, displs, MPI_INT, v_, scounts[rank], MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatterv(arr, scounts, displs, MPI_INT, v_, scounts[rank], MPI_INT, 0, MPI_COMM_WORLD);
-    for (int i = 0; i < scounts[rank] - 1; i++) {
-        if (v_[i] > v_[i + 1])
+    for (int i = displs[rank]; i < displs[rank] + scounts[rank] - 1; i++) {
+        if (v[i] > v[i + 1])
             errors++;
     }
     MPI_Reduce(&errors, &res, procs, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     delete[] displs;
     delete[] scounts;
-    delete[] arr;
     delete[] v_;
     return res;
 }
