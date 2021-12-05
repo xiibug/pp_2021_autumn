10d0e8388ad6c59f90afa2e04a4d7de8bd827000
bloodreina5 <65079069+bloodreina5@users.noreply.github.com>
2021-12-05 20:45:40 +0100

    v2

--- modules/task_1/andrich_m_min_in_rows/min_in_rows.h
+++ modules/task_1/andrich_m_min_in_rows/min_in_rows.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Andrich Maria
-#ifndef MODULES_TASK_1_ANDRICH_M_ROWS_MIN_IN_ROWS_H_
-#define MODULES_TASK_1_ANDRICH_M_ROWS_MIN_IN_ROWS_H_
+#ifndef MODULES_TASK_1_ANDRICH_M_MIN_IN_ROWS_H_
+#define MODULES_TASK_1_ANDRICH_M_MIN_IN_ROWS_H_
 
 #include <vector>
 
@@ -8,4 +8,4 @@ std::vector<int> randomMatrix(int cols, int rows);
 std::vector<int> getParallelOperations(std::vector<int> global_mat, int cols, int rows);
 std::vector<int> getSequentialOperations(std::vector<int> mat, int cols, int rows);
 
-#endif  // MODULES_TASK_1_ANDRICH_M_ROWS_MIN_IN_ROWS_H_
+#endif  // MODULES_TASK_1_ANDRICH_M_MIN_IN_ROWS_H_
