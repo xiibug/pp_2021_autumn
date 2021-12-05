b687ae21298ea4896bff03b4d303bf23fd67f83b
bloodreina5 <65079069+bloodreina5@users.noreply.github.com>
2021-12-05 21:59:40 +0100

    changes2

--- modules/task_1/andrich_m_min_in_rows/main.cpp
+++ modules/task_1/andrich_m_min_in_rows/main.cpp
@@ -5,99 +5,98 @@
 #include <gtest-mpi-listener.hpp>
 
 TEST(Parallel_Operations_MPI, Matrix_16x16) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int rows = 16;
-	int cols = 16;
-	std::vector<int> global_vec;
+	 int rank;
+	 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	 int rows = 16;
+	 int cols = 16;
+	 std::vector<int> global_vec;
 
-	if (rank == 0) {
-		global_vec = randomMatrix(cols, rows);
-	}
+	 if (rank == 0) {
+		 global_vec = randomMatrix(cols, rows);
+	 }
+	 std::vector<int> result = getParallelOperations(global_vec, rows, cols);
 	
-	std::vector<int> result = getParallelOperations(global_vec, rows, cols);
-	
-	if (rank == 0) {
-		std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
-		ASSERT_EQ(control_result, result);
-	}
-}
+     if (rank == 0) {
+	 std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
+		 ASSERT_EQ(control_result, result);
+	  }
+ }
 
 TEST(Parallel_Operations_MPI, Matrix_100x100) {
 	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int rows = 100;
-	int cols = 100;
-	std::vector<int> global_vec;
+	 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	 int rows = 100;
+	 int cols = 100;
+	 std::vector<int> global_vec;
+	 
+	if (rank == 0) {
+	 global_vec = randomMatrix(cols, rows);
+	 }
+	
+	 std::vector<int> result = getParallelOperations(global_vec, rows, cols);
 	
 	if (rank == 0) {
-		global_vec = randomMatrix(cols, rows);
-	}
-	
-	std::vector<int> result = getParallelOperations(global_vec, rows, cols);
-	
-	if (rank == 0) {
-		std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
+	 std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
 		ASSERT_EQ(control_result, result);
-	}
-}
+	 }
+ }
 
 TEST(Parallel_Operations_MPI, Matrix_68x48) {
 	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int rows = 68;
-	int cols = 48;
-	std::vector<int> global_vec;
+	 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	 int rows = 68;
+	 int cols = 48;
+	 std::vector<int> global_vec;
 	
 	if (rank == 0) {
-		global_vec = randomMatrix(cols, rows);
-	}
+	 global_vec = randomMatrix(cols, rows);
+	 }
 	
-	std::vector<int> result = getParallelOperations(global_vec, rows, cols);
+	 std::vector<int> result = getParallelOperations(global_vec, rows, cols);
 	
 	if (rank == 0) {
-		std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
+	 std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
 		ASSERT_EQ(control_result, result);
-	}
-}
+	 }
+ }
 
 TEST(Parallel_Operations_MPI, Matrix_37x1) {
 	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int rows = 37;
-	int cols = 1;
-	std::vector<int> global_vec;
+	 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	 int rows = 37;
+	 int cols = 1;
+	 std::vector<int> global_vec;
 
-	if (rank == 0) {
-		global_vec = randomMatrix(cols, rows);
-	}
+    if (rank == 0) {
+	 global_vec = randomMatrix(cols, rows);
+	 }
 	
-	std::vector<int> result = getParallelOperations(global_vec, rows, cols);
+	 std::vector<int> result = getParallelOperations(global_vec, rows, cols);
 	
 	if (rank == 0) {
-		std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
+	 std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
 		ASSERT_EQ(control_result, result);
-	}
-}
+	 }
+ }
 
 TEST(Parallel_Operations_MPI, Matrix_1x37) {
 	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int rows = 1;
-	int cols = 37;
-	std::vector<int> global_vec;
+	 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	 int rows = 1;
+	 int cols = 37;
+	 std::vector<int> global_vec;
 
 	if (rank == 0) {
-		global_vec = randomMatrix(cols, rows);
-	}
+	 global_vec = randomMatrix(cols, rows);
+	 }
 	
-	std::vector<int> result = getParallelOperations(global_vec, rows, cols);
+	 std::vector<int> result = getParallelOperations(global_vec, rows, cols);
 	
 	if (rank == 0) {
-		std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
+	 std::vector<int> control_result = getSequentialOperations(global_vec, rows, cols);
 		ASSERT_EQ(control_result, result);
-	}
-}
+	 }
+ }
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
