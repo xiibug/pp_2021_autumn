cc5b8c312c2cb9640312fdd4b6100f766c5c412f
KutuevR <roman.kutuev2001@icloud.com>
2021-12-06 20:28:31 +0300

    kutuev_r_reduce

--- modules/task_2/kutuev_r_reduce/reduce.h
+++ modules/task_2/kutuev_r_reduce/reduce.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Kutuev Roman
 
-#ifndef MODULES_TASK_2_KUTUEV_R_MPI_REDUCE_MPI_REDUCE_H_
-#define MODULES_TASK_2_KUTUEV_R_MPI_REDUCE_MPI_REDUCE_H_
+#ifndef MODULES_TASK_2_KUTUEV_R_REDUCE_REDUCE_H_
+#define MODULES_TASK_2_KUTUEV_R_REDUCE_REDUCE_H_
 
 #include <mpi.h>
 
@@ -10,4 +10,4 @@
 
 int my_reduce(void*, void*, int, MPI_Datatype, MPI_Op, int, MPI_Comm);
 
-#endif  //  MODULES_TASK_2_KUTUEV_R_MPI_REDUCE_MPI_REDUCE_H_
+#endif  //  MODULES_TASK_2_KUTUEV_R_REDUCE_REDUCE_H_
