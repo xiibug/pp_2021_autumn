c452c9c0932b52c2d8b84a6d455111dcb3045b84
Ilya Kamenev <ilia.kamenevv@yandex.ru>
2021-12-07 15:36:26 +0300

    fix  style 2

--- modules/task_2/kamenev_i_broadcast/broadcast.h
+++ modules/task_2/kamenev_i_broadcast/broadcast.h
@@ -13,4 +13,4 @@ std::vector<T> getRandomVector(int n, int max);
 int Bcast(void* sendbuf, void* outbuf, int count, MPI_Datatype datatype,
           int root, MPI_Op op, MPI_Comm comm);
 
-#endif  // MODULES_TASK_2_KAMENEV_I_BROADCAST_VEC_SUM_BROADCAST_VEC_SUM_H_
+#endif  // MODULES_TASK_2_KAMENEV_I_BROADCAST_BROADCAST_H_
