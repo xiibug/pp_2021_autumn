dccf59791dedcd4999335ae7e6ee3ddf5d90720a
Александра Троегубова <troeg1306@gmail.com>
2021-12-07 11:46:17 +0300

    fixed

--- modules/task_2/troegubova_a_allreduce/allreduce.h
+++ modules/task_2/troegubova_a_allreduce/allreduce.h
@@ -10,6 +10,6 @@ std::vector<T> getRandomVec(int size);
 
 int ALLreduce(void *sendbuf, void *recvbuf, int count, MPI_Datatype type, MPI_Op op, MPI_Comm comm);
 
-#endif  // MODULES_TASK_1_TROEGUBOVA_A_ALLREDUCE_ALLREDUCE_H_
+#endif  // MODULES_TASK_2_TROEGUBOVA_A_ALLREDUCE_ALLREDUCE_H_
 
 
