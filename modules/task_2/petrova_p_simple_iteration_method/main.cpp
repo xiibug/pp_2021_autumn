b274c4c9219ada43b36d4ec9445dd48fd16b43d1
Polina-Petrova07 <polina_petrova_02@bk.ru>
2021-12-08 21:21:29 +0300

    first commit

---
+++ modules/task_2/petrova_p_simple_iteration_method/main.cpp
@@ -0,0 +1,115 @@
+// Copyright 2021 Petrova Polina
+#include <gtest/gtest.h>
+#include <vector>
+#include "./simple_iteration_method.h"
+#include <gtest-mpi-listener.hpp>
+
+
+TEST(simple_iteration, can_create_random_vector_0) {
+    std::vector<double> a = fillRandomVector(100);
+    ASSERT_NO_THROW();
+}
+TEST(simple_iteration, can_create_random_vector_1) {
+    ASSERT_ANY_THROW(std::vector<double> a = fillRandomVector(-777));
+}
+TEST(simple_iteration, can_create_random_matrix_0) {
+    std::vector< std::vector<double> > a = fillRandomMatrix(100);
+    ASSERT_NO_THROW();
+}
+TEST(simple_iteration, can_create_random_matrix_1) {
+    ASSERT_ANY_THROW(std::vector< std::vector<double> > a = fillRandomMatrix(-777););
+}
+TEST(simple_iteration, test_0) {
+    std::vector< std::vector<double>> A{
+        {4,1,1},
+        {1,6,-1},
+        {1,2,5}
+    };
+    std::vector<double> b{ 9, 10, 20 };
+    std::vector<double> x;
+    std::vector<double> ans{ 1.0001, 2, 3.0001 };
+    x = seqMethod(A, b, 3);
+    for (int i = 0; i < 3; i++) {
+        EXPECT_TRUE((x[i] - ans[i]) < 0.0001);
+    }
+}
+TEST(simple_iteration, test_1) {
+    std::vector< std::vector<double>> A{
+        {4,1,1},
+        {1,6,-1},
+        {1,2,5}
+    };
+    std::vector<double> b{ 9, 10, 20 };
+    std::vector<double> ans{ 1.0001, 2 ,3.0001 };
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    double eps = 0.0001;
+    std::vector<double> x = parallelMethod(A, b, 3);
+    if (procRank == 0)
+        for (int i = 0; i < 3; i++) {
+            EXPECT_TRUE((x[i] - ans[i]) < 0.0001);
+        }
+}
+TEST(simple_iteration, test_2) {
+    std::vector< std::vector<double>> A{
+    {4, 1, 1},
+    {1, 6, -1},
+    {1, 2, 5}
+    };
+    std::vector<double> b{ 9, 10, 20 };
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    double eps = 0.0001;
+    std::vector<double> x = parallelMethod(A, b, 3);
+    if (procRank == 0) {
+        std::vector<double> seqX = seqMethod(A, b, 3);
+        EXPECT_EQ(x, seqX);
+    }
+}
+TEST(simple_iteration, test_3) {
+    std::vector< std::vector<double> >A;
+    std::vector<double> b;
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        A = fillRandomMatrix(5);
+        b = fillRandomVector(5);
+    }
+    double eps = 0.01;
+    std::vector<double> x = parallelMethod(A, b, 3);
+    if (procRank == 0) {
+        EXPECT_NO_THROW();
+    }
+
+}
+TEST(simple_iteration, test_4) {
+    std::vector< std::vector<double> >A;
+    std::vector<double> b;
+    int procRank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+    if (procRank == 0) {
+        A = fillRandomMatrix(33);
+        b = fillRandomVector(33);
+    }
+    double eps = 0.01;
+    std::vector<double> x = parallelMethod(A, b, 3);
+    if (procRank == 0) {
+        std::vector<double> xSeq = seqMethod(A, b, 3);
+        EXPECT_EQ(x, xSeq);
+    }
+
+}
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_2/petrova_p_simple_iteration_method/simple_iteration_method.cpp
@@ -0,0 +1,147 @@
+// Copyright 2021 Petrova Polina
+#include <mpi.h>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <cstdlib> 
+#include"../../../modules/task_2/petrova_p_simple_iteration_method/simple_iteration_method.h"
+
+std::vector<double> fillRandomVector(int len) {
+    if (len < 0) {
+        throw "error! lenght vector < 0!";
+    }
+    else {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dist(1, 10);  // values 1-100 // check commit
+        std::vector<double> v(len);
+        for (int i = 0; i < len; i++) {
+            v[i] = dist(gen);
+        }
+        return v;
+    }
+}
+std::vector< std::vector<double>> fillRandomMatrix(int n) {
+    if (n < 0) {
+        throw "error! lenght vector < 0!";
+    }
+    else {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dist(1, 10);  // values 1-10 // check commit
+        std::uniform_int_distribution<> dist1(10, 20); //for diagonal
+        std::vector<std::vector<double>> v(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    v[i].push_back(dist1(gen));
+                v[i].push_back(dist(gen));
+            }
+        }
+        return v;
+    }
+}
+std::vector<double> seqMethod(std::vector< std::vector<double>> mat, std::vector<double> b, int n) {
+    //n = mat.size();
+    std::vector<double> firstSolv(n, 0.0);
+    int it = 0;
+    double eps = 0.0001;
+    while (true) {
+        it++;
+        if (it == 1000)
+            break;
+        std::vector<double> currentSol(n, 0.0);
+        for (int i = 0; i < n; i++) {
+            currentSol[i] = mat[i][n] / mat[i][i];
+            for (int j = 0; j < n; j++) {
+                if (i != j)
+                    currentSol[i] = currentSol[i] - mat[i][j] / mat[i][i] * firstSolv[i];
+            }
+        }
+        double err = 0.0;
+        for (int i = 0; i < n; i++) {
+            err = err + abs(currentSol[i] - firstSolv[i]);
+        }
+        if (err < eps)
+            break;
+        firstSolv = currentSol;
+    }
+    return firstSolv;
+}
+std::vector<double> parallelMethod(std::vector<std::vector <double> > mat, std::vector<double> b, int n) {
+    int procNum, procRank;
+    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
+
+    if ((n < procNum) || (procNum == 1)) {
+        if (procRank == 0) {
+            return seqMethod(mat, b, n);
+        }
+        else {
+            return std::vector<double>();
+        }
+    }
+    std::vector<double> firstSolv(n, 0.0);
+    int littleLen = n / procNum;
+    int startI;
+    std::vector< std::vector<double> > locMat;
+    int count = littleLen;
+    if (procRank == 0) {
+        locMat = mat;
+        locMat.resize(mat[0][0] + littleLen * n);
+        for (int i = 1; i < procNum; i++) {
+            startI = littleLen * i;
+            if (i == procNum - 1)
+                count = n - startI;
+            MPI_Send(mat[startI].data(), n * count, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        }
+    }
+    else {
+        MPI_Status status;
+        if (procNum = procNum - 1)
+            count = n - littleLen * (procNum - 1);
+        locMat = mat;
+        locMat.resize(count);
+        MPI_Recv(locMat[0].data(), n * count, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+    }
+    std::vector<int> resCount(procNum);
+    std::vector<int> extra(procNum);
+    for (int i = 0; i < procNum; i++) {
+        if (i == procNum - 1)
+            resCount[i] = n - littleLen * (procNum - 1);
+        else
+            resCount[i] = littleLen;
+        if (i == 0)
+            extra[i] = 0;
+        else
+            extra[i] = extra[i - 1] + littleLen;
+    }
+
+    int it = 0;
+    double eps = 0.0001;
+    while (true) {
+        it++;
+        if (it == 1000) {
+            break;
+        }
+        std::vector<double> currentSolv(n, 0.0);
+        for (int i; i < locMat.size(); i++) {
+            currentSolv[i] = locMat[i][locMat.size() - 1] / locMat[i][i + procRank * littleLen];
+            for (int j = 0; j < locMat.size() - 1; j++) {
+                if (i + procRank * littleLen != j)
+                    currentSolv[i] = currentSolv[i] - (locMat[i][j] / locMat[i][i + procRank * littleLen]) * firstSolv[j];
+            }
+        }
+        double err = 0.0;
+        for (int i = 0; i < locMat.size(); i++) {
+            err = err + abs(currentSolv[i] - firstSolv[i + procRank * littleLen]);
+        }
+        MPI_Allreduce(MPI_IN_PLACE, &err, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        if (err < eps) {
+            break;
+        }
+        MPI_Allgatherv(currentSolv.data(), count, MPI_DOUBLE, firstSolv.data(),
+            resCount.data(), extra.data(), MPI_DOUBLE, MPI_COMM_WORLD);
+    }
+    return firstSolv;
+}
