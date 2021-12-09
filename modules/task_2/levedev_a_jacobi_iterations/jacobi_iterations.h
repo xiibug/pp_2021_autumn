676aa63d6d8b99e370077f1d6965ec14197fcd56
AlexeyLebedev1 <alexey.lebedev@intel.com>
2021-12-09 11:25:24 +0300

    create template

---
+++ modules/task_2/levedev_a_jacobi_iterations/jacobi_iterations.h
@@ -0,0 +1,11 @@
+// Copyright 2021 Lebedev Alexey
+#ifndef MODULES_TASK_2_LEBEDEV_A_JACOBI_ITERATIONS_H_
+#define MODULES_TASK_2_LEBEDEV_A_JACOBI_ITERATIONS_H_
+
+#include <string>
+
+std::string getRandomString(size_t size);
+int UniqueCharsSequential(const std::string& str1, const std::string& str2);
+int UniqueCharsParallel(const std::string& str1, const std::string& str2);
+
+#endif  // MODULES_TASK_2_LEBEDEV_A_JACOBI_ITERATIONS_H_
