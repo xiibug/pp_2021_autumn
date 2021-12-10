7011852c042e8bf233e244ae44d06dfc8ecf2fc0
danclas <danvershinin@gmail.com>
2021-12-10 11:55:47 +0300

    First commit

---
+++ modules/task_1/vershinin_d_vector_max_diff/vector_max_diff.h
@@ -0,0 +1,11 @@
+// Copyright 2021 Vershinin Daniil
+#ifndef MODULES_TASK_1_VERSHININ_D_VECTOR_MAX_DIFF_VECTOR_MAX_DIFF_H_
+#define MODULES_TASK_1_VERSHININ_D_VECTOR_MAX_DIFF_VECTOR_MAX_DIFF_H_
+
+#include <vector>
+
+std::vector<int> getRandomVector(int sz);
+int getParallelOperations(std::vector<int> global_vec);
+int getSequentialOperations(std::vector<int> vec);
+
+#endif  // MODULES_TASK_1_VERSHININ_D_VECTOR_MAX_DIFF_VECTOR_MAX_DIFF_H_
