9d9ac22feacf9557b9e4346b40e337982df7669d
Nikolai Groshev <kolia.avatar@gmail.com>
2021-12-10 17:02:09 +0300

    code style fixes

--- modules/task_2/groshev_n_reduce/reduce.h
+++ modules/task_2/groshev_n_reduce/reduce.h
@@ -3,11 +3,17 @@
 #define MODULES_TASK_2_GROSHEV_N_REDUCE_REDUCE_H_
 
 #include <mpi.h>
+#include <algorithm>
 
-void my_reduce(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm);
-void sum(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm, int my_rank);
-void prod(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm, int my_rank);
-void max(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm, int my_rank);
-void min(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MPI_Op op, int root, MPI_Comm comm, int my_rank);
+void my_reduce(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+               MPI_Op op, int root, MPI_Comm comm);
+void sum(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+         MPI_Op op, int root, MPI_Comm comm, int my_rank);
+void prod(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+          MPI_Op op, int root, MPI_Comm comm, int my_rank);
+void max(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+         MPI_Op op, int root, MPI_Comm comm, int my_rank);
+void min(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+         MPI_Op op, int root, MPI_Comm comm, int my_rank);
 
 #endif  //  MODULES_TASK_2_GROSHEV_N_REDUCE_REDUCE_H_
