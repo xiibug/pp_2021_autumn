7354551a2fdd414c33147a6c30b49e0f569e7f00
KimNikita <ghj710@mail.ru>
2021-12-11 00:42:37 +0300

    fix const ref

--- modules/task_3/kim_n_sobel/sobel.h
+++ modules/task_3/kim_n_sobel/sobel.h
@@ -12,8 +12,8 @@ int calcNewPixelColor(std::vector<std::vector<int>> image, int x, int y, int siz
 
 int calcNewPixelColor(std::vector<int> image, int x, int y, int size, int kernel[3][3]);
 
-std::vector<std::vector<int>> getSobelImage(std::vector<std::vector<int>> image, int size, char type);
+std::vector<std::vector<int>> getSobelImage(const std::vector<std::vector<int>>& image, int size, char type);
 
-std::vector<std::vector<int>> getSobelImageParall(std::vector<std::vector<int>> image, int size, char type);
+std::vector<std::vector<int>> getSobelImageParall(const std::vector<std::vector<int>>& image, int size, char type);
 
 #endif  // MODULES_TASK_3_KIM_N_SOBEL_SOBEL_H_
