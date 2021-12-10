6e673d94a6d1140caa7d04ffc691fdda62333cff
pichugin-i <pichugin.lua@yandex.ru>
2021-12-10 23:29:05 +0300

    first commit

---
+++ modules/task_1/pichugin_i_matrix_sum_cols/matrix_sum_cols.h
@@ -0,0 +1,8 @@
+// Copyright 2021 Pichugin Ilya
+#ifndef MODULES_TASK_1_PICHUGIN_I_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
+#define MODULES_TASK_1_PICHUGIN_I_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
+int* CreateRandMatrix(int rows, int cols);
+int* LinearMetod(int* Matrix, int rows, int cols);
+int* MPIMethod(int* M, int cs, int rs);
+
+#endif  // MODULES_TASK_1_PICHUGIN_I_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
