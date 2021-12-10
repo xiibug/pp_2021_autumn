9b928ced34418d6dfeab452c307baadc627e48c0
brimst0ne <artemchelebaev@gmail.com>
2021-12-10 15:29:58 +0300

    Tab fix

--- modules/task_2/chelebaev_a_gather/gather.h
+++ modules/task_2/chelebaev_a_gather/gather.h
@@ -10,6 +10,6 @@ template <class T>
 std::vector<T> getRandomVector(int size);
 
 int Gather(void *sbuf, int scount, MPI_Datatype stype, void *rbuf, int rcount,
-	MPI_Datatype rtype, int root, MPI_Comm comm);
+MPI_Datatype rtype, int root, MPI_Comm comm);
 
 #endif  // MODULES_TASK_2_CHELEBAEV_A_GATHER_GATHER_H_
