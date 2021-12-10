715597c7481b5728df12568ee90a78b21fbaa790
ABoyarskov <anboyarskov@yandex.ru>
2021-12-10 17:48:09 +0300

    fix style

--- modules/task_2/boyarskov_a_image_smoothing/image_smoothing.h
+++ modules/task_2/boyarskov_a_image_smoothing/image_smoothing.h
@@ -1,9 +1,9 @@
 // Copyright 2021 Boyarskov Anton
-#ifndef MODULES_TASK_2_BOYARSKON_A_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
-#define MODULES_TASK_2_BOYARSKON_A_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
+#ifndef MODULES_TASK_2_BOYARSKOV_A_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
+#define MODULES_TASK_2_BOYARSKOV_A_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
 
 int* getRandomImage(int width, int height);
 int* getSequentialGauss(int* image, int width, int height, int radius);
 int* getParallelGauss(int* image, int width, int height, int radius);
 
-#endif  // MODULES_TASK_2_BOYARSKON_A_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
+#endif  // MODULES_TASK_2_BOYARSKOV_A_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
