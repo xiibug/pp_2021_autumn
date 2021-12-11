b04da087af38052aa758d4d598e032b429ed9829
pichugin-i <pichugin.lua@yandex.ru>
2021-12-11 11:37:58 +0300

    comm

--- modules/task_1/pichugin_i_matrix_sum_cols/matrix_sum_cols.h
+++ modules/task_1/pichugin_i_matrix_sum_cols/matrix_sum_cols.h
@@ -3,6 +3,6 @@
 #define MODULES_TASK_1_PICHUGIN_I_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
 int* CreateRandMatrix(int rows, int cols);
 int* LinearMetod(int* Matrix, int rows, int cols);
-int* MPIMethod(int* M, int rs, int cs);
+int* MPIMethod(int* Matrix_init, int rs, int cs);
 
 #endif  // MODULES_TASK_1_PICHUGIN_I_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
