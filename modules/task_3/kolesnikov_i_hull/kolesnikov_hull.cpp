3da5d815ece6ff28aebc4db604ea7d3f31f26aa5
Ilya <eeemeeny@gmail.com>
2021-12-16 17:13:49 +0300

    init

---
+++ modules/task_3/kolesnikov_i_hull/kolesnikov_hull.cpp
@@ -0,0 +1,279 @@
+// Copyright 2021 Kolesnikov Ilya
+#include <iostream>
+#include "../../../modules/task_3/kolesnikov_i_hull/kolesnikov_hull.h"
+
+void swap_p(Point& a, Point& b) {
+    Point a_tmp(0, 0);
+    a_tmp = a;
+    a = b;
+    b = a_tmp;
+}
+
+std::vector<Point> sort_vec(std::vector<Point>& vec) {
+    for (int i(0); i < vec.size(); ++i) {
+        for (int j(0); j < vec.size() - 1; ++j) {
+            if ((vec[j].returnX() > vec[j + 1].returnX())) {
+                swap_p(vec[j], vec[j + 1]);
+            }
+        }
+    }
+
+    for (int i(0); i < vec.size(); ++i) {
+        for (int j(0); j < vec.size() - 1; ++j) {
+            if ((vec[j].returnX() == vec[j + 1].returnX()) && (vec[j].returnY() > vec[j + 1].returnY())) {
+                swap_p(vec[j], vec[j + 1]);
+            }
+        }
+    }
+
+    return vec;
+}
+
+int orientation(Point p, Point q, Point r) {
+    int res = (q.returnY() - p.returnY()) * (r.returnX() - q.returnX()) - (r.returnY() - q.returnY()) * (q.returnX() - p.returnX());
+    return res;
+}
+
+int get_pre_last(std::vector<Point> vec) {
+    int res = 0;
+    bool check = false;
+    int x = vec[1].returnX() - vec[0].returnX();
+    int y = vec[1].returnY() - vec[0].returnY();
+    for (int i(2); i < vec.size(); ++i) {
+        if (vec[i].returnX() - vec[i - 1].returnX() == x && vec[i].returnY() - vec[i - 1].returnY() == y) {
+            check = true;
+        }
+        else {
+            check = false;
+            break;
+        }
+    }
+    if (check) {
+        return vec.size() - 1;
+    }
+    for (int i(1); i < vec.size(); ++i) {
+        if (vec[0].returnX() == vec[i].returnX()) {
+            return i;
+        }
+    }
+    Point pt(0,0);
+    if (orientation(vec[0], vec[1], vec[2]) <= 0) {
+        if (vec[1].returnY() >= vec[0].returnY()) {
+            return 1;
+        }
+    }
+
+    return res;
+}
+
+std::vector<int> point_to_int(std::vector<Point> vec) {
+    std::vector<int> res;
+    for (auto pt : vec) {
+        res.push_back(pt.returnX());
+        res.push_back(pt.returnY());
+    }
+
+    return res;
+}
+
+std::vector<Point> int_to_point(std::vector<int> vec) {
+    std::vector<Point> res;
+    for (int i(0); i < vec.size()-1; i+=2) {
+         res.push_back(Point(vec[i], vec[i + 1]));
+    }
+
+    return res;
+}
+
+std::vector<Point> convexHull_jarvis_parallel(std::vector<Point> vec) {
+    std::vector<Point> steps;
+    int size = 0, rank = 0;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Status status;
+    int l = 0;
+    int n = vec.size();
+    for (int i = 0; i < n; i++) {
+
+        if (vec[i].returnX() < vec[l].returnX()) {
+            l = i;
+        }
+    }
+    int  vec_size = vec.size();
+    
+    int x = l, y;
+    int range = 6;
+    int size_s = 0;
+
+    if (rank != 0) {
+        range = 6;
+        MPI_Recv(&x, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+    }
+    else if (size == 1) {
+        range = vec.size();
+    }
+    else {
+    }
+    int cnt = 0;
+    int pre_last = get_pre_last(vec);
+    int x_tmp = 0;
+
+    if (rank != size - 1) {
+        do {
+            if (x == pre_last) {
+                break;
+            }
+            if (rank != size - 1 && vec[x] == vec[0] && cnt > 0) {
+                break;
+            }
+            if (cnt == range - 1) {
+                x_tmp = x;
+            }
+            if (cnt <= range) {
+                if (cnt + 1 < range) {
+                    steps.push_back(vec[x]);
+                }
+            }
+            if (cnt > range) {
+                break;
+            }
+
+            y = (x + 1) % n;
+
+            for (int i = 0; i < n; i++) {
+                if (i + 1 < n) {
+                    if (vec[i] == vec[i + 1]) {
+                        continue;
+                    }
+                }
+                if (orientation(vec[x], vec[i], vec[y]) < 0) {
+                    y = i;
+                }
+            }
+
+            x = y;
+            cnt++;
+
+        } while (x != l);
+    }
+    else {
+        do {
+            steps.push_back(vec[x]);
+            y = (x + 1) % n;
+            for (int i = 0; i < n; i++) {
+                if (i + 1 < n) {
+                    if (vec[i] == vec[i + 1]) {
+                        continue;
+                    }
+                }
+                if (orientation(vec[x], vec[i], vec[y]) < 0) {
+                    y = i;
+                }
+            }
+            x = y;
+            cnt++;
+            if (x == pre_last) {
+                steps.push_back(vec[x]);
+                break;
+            }
+        } while (x != l);
+    }
+
+    if (rank != size - 1) {
+        MPI_Send(&x_tmp, 1, MPI_INT, rank + 1, 1, MPI_COMM_WORLD);
+    }
+    else {
+        if (steps[0] != steps[steps.size() - 1]) {
+            steps.push_back(vec[0]);
+        }
+    }
+
+    std::vector<int> real_points = point_to_int(steps);
+    std::vector<int> send_counts(size);
+    int size_to_send = real_points.size();
+    MPI_Gather(&size_to_send, 1, MPI_INT, send_counts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Bcast(send_counts.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    std::vector <int> dspls;
+    int sum_p = 0;
+    for (int i(0); i < size; ++i) {
+        dspls.push_back(sum_p);
+        sum_p += send_counts[i];
+    }
+    std::vector<int> steps_to_send(sum_p);
+    MPI_Gatherv(real_points.data(), real_points.size(), MPI_INT, steps_to_send.data(), send_counts.data(), dspls.data(), MPI_INT, 0, MPI_COMM_WORLD);
+    std::vector<Point>steps_to_send2 = int_to_point(steps_to_send);
+    std::vector<Point>steps_to_send3;
+    steps_to_send3.push_back(steps_to_send2[0]);
+    for (int i(1); i < steps_to_send2.size(); ++i) {
+        if (steps_to_send2[i] == steps_to_send2[0]) {
+            steps_to_send3.push_back(steps_to_send2[i]);
+            break;
+        }
+        steps_to_send3.push_back(steps_to_send2[i]);
+    }
+
+    return steps_to_send3;
+}
+
+std::vector<Point> convexHull_jarvis(std::vector<Point> vec) {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::ofstream points, hull;
+    points.open("points.txt");
+    hull.open("hull.txt");
+
+    std::vector<Point> steps;
+
+    int l = 0;
+    int n = vec.size();
+    for (int i = 0; i < n; i++) {
+
+        if (vec[i].returnX() < vec[l].returnX()) {
+            l = i;
+        }
+    }
+
+    int x = l, y;
+    int pre_last = get_pre_last(vec);
+    do {
+        steps.push_back(vec[x]);
+        y = (x + 1) % n;
+
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n) {
+                if (vec[i] == vec[i + 1]) {
+                    continue;
+                }
+            }
+            if (orientation(vec[x], vec[i], vec[y]) < 0) {
+                y = i;
+            }
+        }
+
+        x = y;
+        if (x == pre_last) {
+            steps.push_back(vec[x]);
+            break;
+        }
+
+    } while (x != l);
+    if (steps[0] != steps[steps.size() - 1]) {
+        steps.push_back(vec[0]);
+    }
+
+    for (int i(0); i < vec.size(); ++i) {
+        points << vec[i].returnX() << " " << vec[i].returnY() << "\n";
+    }
+    for (int i(0); i < steps.size(); ++i) {
+        hull << steps[i].returnX() << " " << steps[i].returnY() << "\n";
+    }
+    points.close();
+    hull.close();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto dt = ((std::chrono::nanoseconds)(t2 - t1)).count();
+
+    return steps;
+}
---
+++ modules/task_3/kolesnikov_i_hull/kolesnikov_hull.h
@@ -0,0 +1,48 @@
+// Copyright 2021 Kolesnikov Ilya
+#ifndef MODULES_TASK_3_KOLESNIKOV_I_HULL_KOLESNIKOV_HULL_H_
+#define MODULES_TASK_3_KOLESNIKOV_I_HULL_KOLESNIKOV_HULL_H_
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
+MPI_Comm topology_ring(const MPI_Comm comm);
+
+int single_find_symbol(char symbol, std::string str);
+int parallel_find_symbol(char symbol, std::string str);
+
+class Point {
+public:
+    Point(int k, int l) { x = k; y = l; }
+    Point(Point* pt) { x = pt->returnX(); y = pt->returnY();
+    }
+    int returnX() { 
+        return this->x; 
+    }
+    int returnY() { 
+        return this->y; 
+    }
+    bool operator!=(Point& other) {
+        return ((other.returnX()!=this->x)&&(other.returnY()!=this->y) || (other.returnX() == this->x) && (other.returnY() != this->y) || (other.returnX() != this->x) && (other.returnY() == this->y))?true:false;
+    }
+    bool operator==(Point& other) {
+        return (other.returnX() == this->x) && (other.returnY() == this->y)?true:false;
+    }
+    Point operator-(Point& p) {
+        return Point(this->x - p.returnX(), this->y - p.returnY());
+    }
+
+private:
+    int x, y;
+};
+
+void swap_p(Point& a, Point& b);
+std::vector<Point> sort_vec(std::vector<Point>& vec);
+int orientation(Point p, Point q, Point r);
+int get_pre_last(std::vector<Point> vec);
+std::vector<int> point_to_int(std::vector<Point> vec);
+std::vector<Point> int_to_point(std::vector<int> vec);
+std::vector<Point> convexHull_jarvis(std::vector<Point> vec);
+std::vector<Point> convexHull_jarvis_parallel(std::vector<Point> vec);
+
+#endif  // MODULES_TASK_3_KOLESNIKOV_I_HULL_KOLESNIKOV_HULL_H_
