c750077a75b1da12ec40dcd8d8a532dc4d1a36a0
Maksim Popp <popp.m@bk.ru>
2021-12-16 16:42:02 +0300

    task1

---
+++ modules/task_1/popp_m_max_value_of_matrix/max_value_of_matrix.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Popp Maksim
+#ifndef MODULES_TASK_1_POPP_M_MAX_VALUE_OF_MATRIX_MAX_VALUE_OF_MATRIX_H_
+#define MODULES_TASK_1_POPP_M_MAX_VALUE_OF_MATRIX_MAX_VALUE_OF_MATRIX_H_
+
+int* generateMatrixRandom(int k, int l);
+int searchMaxSequentially(int* arr, int k, int l);
+int findingMaxInParallel(int* arr, int k, int l);
+
+#endif  // MODULES_TASK_1_POPP_M_MAX_VALUE_OF_MATRIX_MAX_VALUE_OF_MATRIX_H_
