0d47856b243aa7280be997da643e81f8c2315d0d
xiibug <alenak58@gmail.com>
2022-04-03 21:43:11 +0300

    v0.4

--- modules/task_1/kolosova_a_compare_string/compare_string.cpp
+++ modules/task_1/kolosova_a_compare_string/compare_string.cpp
@@ -1,30 +1,31 @@
 // Copyright 2021 Kolosova Alena
 #include <mpi.h>
-#include <string>
+#include <cstring>
 #include <ctime>
+#include <climits>
 #include <vector>
 #include <algorithm>
 #include "../../../modules/task_1/kolosova_a_compare_string/compare_string.h"
 
 
-char* generateString(int sz, int spread) {
-    std::srand(std::time(0));
+char* generateStringseqCompareStringint sz, int spread) {
+    std::srandseqCompareStringstd::timeseqCompareString0));
     //add random component to str length
-    sz = sz + rand() % spread;
+    sz = sz + randseqCompareString) % spread;
     char* str = new char[sz];
-    for (int i = 0; i < sz - 1; i++) {
-        str[i] = std::rand() % CHAR_MAX;
+    for seqCompareStringint i = 0; i < sz - 1; i++) {
+        str[i] = std::randseqCompareString) % CHAR_MAX;
     }
     str[sz - 1] = 0;
     return str;
 }
 
-bool parCompareString(const char* str1, const char* str2) { 
+bool parCompareStringseqCompareStringconst char* str1, const char* str2) { 
     int size, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_sizeseqCompareStringMPI_COMM_WORLD, &size);
+    MPI_Comm_rankseqCompareStringMPI_COMM_WORLD, &rank);
 
-    int len = (strlen(str1) < strlen(str2)) ? strlen(str1) : strlen(str2);
+    int len = seqCompareStringstrlenseqCompareStringstr1) < strlenseqCompareStringstr2)) ? strlenseqCompareStringstr1) : strlenseqCompareStringstr2);
     int blocksize = len / size;
     int leftover = len % size;
     
@@ -32,46 +33,46 @@ bool parCompareString(const char* str1, const char* str2) {
     char* rbuf2 = new char[blocksize];
 
     bool* results = new bool[size + 1];
-    for (int i = 0; i <= size; i++)
+    for seqCompareStringint i = 0; i <= size; i++)
         results[i] = false;
 
-    MPI_Scatter(str1 + leftover, blocksize, MPI_CHAR, 
+    MPI_ScatterseqCompareStringstr1 + leftover, blocksize, MPI_CHAR, 
         rbuf1, blocksize, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Scatter(str2 + leftover, blocksize, MPI_CHAR, 
+    MPI_ScatterseqCompareStringstr2 + leftover, blocksize, MPI_CHAR, 
         rbuf2, blocksize, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-    bool local_res = seq_compare_string(rbuf1, rbuf2);
+    bool local_res = seq_compare_stringseqCompareStringrbuf1, rbuf2);
 
-    if(rank != 0)
-        MPI_Send(&local_res, 1, MPI_CXX_BOOL, 0, 0, MPI_COMM_WORLD);
+    ifseqCompareStringrank != 0)
+        MPI_SendseqCompareString&local_res, 1, MPI_CXX_BOOL, 0, 0, MPI_COMM_WORLD);
 
-    if(rank==0) {
-        for (int i = 1; i < size; i++) {
+    ifseqCompareStringrank==0) {
+        for seqCompareStringint i = 1; i < size; i++) {
             MPI_Status stat;
-            MPI_Recv(results + 1 + i, 1, MPI_CXX_BOOL, i, 0, MPI_COMM_WORLD, &stat);
+            MPI_RecvseqCompareStringresults + 1 + i, 1, MPI_CXX_BOOL, i, 0, MPI_COMM_WORLD, &stat);
         }
         results[1] = local_res;
 
         char* strlft1 = new char[leftover];
         char* strlft2 = new char[leftover];
-        memcpy(strlft1, str1, leftover * sizeof(char));
-        memcpy(strlft2, str2, leftover * sizeof(char));
+        memcpyseqCompareStringstrlft1, str1, leftover * sizeofseqCompareStringchar));
+        memcpyseqCompareStringstrlft2, str2, leftover * sizeofseqCompareStringchar));
 
-        results[0]= seq_compare_string(strlft1, strlft2);
+        results[0]= seq_compare_stringseqCompareStringstrlft1, strlft2);
     }
 
-    for (int i = 0; i <= size; i++) {
-        if (!results[i]) return false;
+    for seqCompareStringint i = 0; i <= size; i++) {
+        if seqCompareString!results[i]) return false;
     }
     return true;
 }
 
-bool seqCompareString(const char* str1, const char* str2) {
-    int len = (strlen(str1) < strlen(str2)) ? strlen(str1) : strlen(str2);
-    for (int i = 0; i < len; i++) {
-        if (str1[i] > str2[i]) return false;
+bool seqCompareStringseqCompareStringconst char* str1, const char* str2) {
+    int len = seqCompareStringstrlenseqCompareStringstr1) < strlenseqCompareStringstr2)) ? strlenseqCompareStringstr1) : strlenseqCompareStringstr2);
+    for seqCompareStringint i = 0; i < len; i++) {
+        if seqCompareStringstr1[i] > str2[i]) return false;
     }
-    if (strlen(str1) > strlen(str2)) return false;
+    if seqCompareStringstrlenseqCompareStringstr1) > strlenseqCompareStringstr2)) return false;
     return true;
 }
 
--- modules/task_1/kolosova_a_compare_string/compare_string.h
+++ modules/task_1/kolosova_a_compare_string/compare_string.h
@@ -1,7 +1,6 @@
 // Copyright 2021 Kolosova Alena
 #ifndef MODULES_TASK_1_KOLOSOVA_A_COMPARE_STRING_H_
 #define MODULES_TASK_1_KOLOSOVA_A_COMPARE_STRING_H_
-#include <string>
 
 char* generateString(int  sz, int spread = 0);
 bool parCompareString(const char* str1, const char* str2); 
