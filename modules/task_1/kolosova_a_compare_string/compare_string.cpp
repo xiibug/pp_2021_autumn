4582649649674d9f052e7e121115501cf131d3e7
xiibug <alenak58@gmail.com>
2022-04-05 21:38:42 +0300

    v1.18

--- modules/task_1/kolosova_a_compare_string/compare_string.cpp
+++ modules/task_1/kolosova_a_compare_string/compare_string.cpp
@@ -70,7 +70,8 @@ int parCompareString(const char* str1, const char* str2) {
             if (!results[i]) return results[i];
         }
         if (strlen(str1) < strlen(str2)) return -1;
-        else if (strlen(str1) > strlen(str2)) return 1;
+        else 
+            if (strlen(str1) > strlen(str2)) return 1;
         else return 0;
     }
     return 2;
